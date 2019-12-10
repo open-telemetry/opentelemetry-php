d9244d63a1ad807a99766c74c21d92272fdc13ee
Bob Strecansky <bob.strecansky@gmail.com>
2019-12-10 13:48:22 -0500

    Performing Always and Never Sample Steps (#30)
    * Performing Always and Never Sample Steps
    
    * Updating examples to remove builder
    
    * Updating examples to remove builder
    
    * Removing builder from always off trace example

---
+++ src/Tracing/Sampler/AlwaysOnSampler.php
@@ -0,0 +1,22 @@
+<?php
+namespace OpenTelemetry\Tracing\Sampler;
+/**
+ * This implementation of the SamplerInterface always returns true.
+ * Example:
+ * ```
+ * use OpenTelemetry\Traceing\Sampler\AlwaysSampleSampler;
+ * $sampler = new AlwaysSampleSampler();
+ * ```
+ */
+class AlwaysOnSampler implements SamplerInterface
+{
+    /**
+     * Returns true because we always want to sample.
+     *
+     * @return bool
+     */
+    public function shouldSample()
+    {
+        return true;
+    }
+} 
