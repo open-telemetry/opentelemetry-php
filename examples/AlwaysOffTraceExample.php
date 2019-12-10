18912901ad075b11601b8055ff44cabeacaa66db
Bob Strecansky <bob.strecansky@gmail.com>
2019-12-10 13:45:48 -0500

    Removing builder from always off trace example

--- examples/AlwaysOffTraceExample.php
+++ examples/AlwaysOffTraceExample.php
@@ -1,5 +1,6 @@
 <?php
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+
 use OpenTelemetry\Tracing\SpanContext;
 use OpenTelemetry\Tracing\Sampler\AlwaysOffSampler;
 use OpenTelemetry\Tracing\Tracer;
@@ -8,12 +9,12 @@ $sampler = AlwaysOffSampler::shouldSample();
 if ($sampler) {
     $spanContext = SpanContext::generate(); // or extract from headers
     $tracer = new Tracer($spanContext);
-    
+
     // start a span, register some events
     $span = $tracer->createSpan('session.generate');
-    $span->setAttributes([ 'remote_ip' => '1.2.3.4' ]);
+    $span->setAttributes(['remote_ip' => '1.2.3.4']);
     $span->setAttribute('country', 'USA');
-    
+
     $span->addEvent('found_login', [
       'id' => 12345,
       'username' => 'otuser',
@@ -21,9 +22,9 @@ if ($sampler) {
     $span->addEvent('generated_session', [
       'id' => md5(microtime(true))
     ]);
-    
+
     $span->end(); // pass status as an optional argument
     print_r($span);  // print the span as a resulting output
 } else {
     echo "Sampling is not enabled";
-} 
+}
