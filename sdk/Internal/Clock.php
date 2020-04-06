22d40025bbd15a3d79dee44ed1beae7af0a4c0d0
Beniamin <beniamin.calota@gmail.com>
2020-04-06 20:54:03 +0300

    fix internal clock returning invalid milliseconds string (#75)

--- sdk/Internal/Clock.php
+++ sdk/Internal/Clock.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\Sdk\Internal;
 
+use function microtime;
+
 class Clock
 {
     public function __construct()
@@ -12,16 +14,8 @@ class Clock
 
     public function millitime(): string
     {
-        return self::format_microtime_to_millitime(\microtime());
-    }
+        $millitime = microtime(true) * 1000;
 
-    public static function format_microtime_to_millitime(string $microtime): string
-    {
-        $space_at = \strpos($microtime, ' ');
-        $decimal = (float) \substr($microtime, 0, $space_at);
-        $seconds = \substr($microtime, $space_at + 1);
-        $milliseconds = (string) ($decimal * 1000);
-
-        return "{$seconds}${milliseconds}";
+        return (string) $millitime;
     }
 }
