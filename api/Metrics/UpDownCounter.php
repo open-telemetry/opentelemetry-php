e52eda9cd2e314395c2edd04e4ca59cea4d9b8f4
Amber Sistla <ambersistla@gmail.com>
2020-08-14 07:10:58 -0700

    Implement an UpDownCounter Instrument for the Metrics API (#157)
    * * Initial UpDownCounter commit.
    * Unit tests included.
    
    * Rebased and addressed merge conflicts.
    
    * Addressed a merge item.
    
    * Fixed formatting.

--- api/Metrics/UpDownCounter.php
+++ api/Metrics/UpDownCounter.php
@@ -4,22 +4,28 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\Metrics;
 
-interface UpDownCounter extends Counter
+/*
+ * Name: UpDownCounter
+ * Instrument kind : Synchronous additive
+ * Function(argument) : Add(increment) where increment is a numeric value
+ * Default aggregation : Sum
+ * Notes : Per-request, part of a non-monotonic sum
+ *
+ * UpDownCounter supports negative increments. This makes UpDownCounter
+ * not useful for computing a rate aggregation. It aggregates a Sum,
+ * only the sum is non-monotonic. It is generally useful for capturing changes
+ * in an amount of resources used, or any quantity that rises and falls during
+ * a request.
+ *
+ */
+interface UpDownCounter
 {
     /**
-     * Subtracts counter value
+     * Updates counter value with the positive or negative int that is passed in.
      *
      * @access	public
-     * @param	int	$value
-     * @return	UpDownCounter
+     * @param	$increment
+     * @return	int returns the non-monotonic sum
      */
-    public function subtract(int $value) : UpDownCounter;
-
-    /**
-     * Decrements counter value
-     *
-     * @access	public
-     * @return	UpDownCounter
-     */
-    public function decrement() : UpDownCounter;
+    public function add($increment) : int;
 }
