ac0e45830d8304eab7be098e7ee8dc5b18f70bb3
Fahmy Mohammed <fmohammed@newrelic.com>
2020-08-18 11:00:46 -0700

    add meter interface

--- sdk/Metrics/Meter.php
+++ sdk/Metrics/Meter.php
@@ -8,5 +8,68 @@ use OpenTelemetry\Metrics as API;
 
 class Meter implements API\Meter
 {
-    // Temp stub
+    /**
+     * @var string            $name
+     * @var string            $version
+     * @var API\Counter       $counter
+     * @var API\UpDownCounter $upDownCounter
+     * @var API\ValueRecorder $valueRecorder
+     */
+    private $name;
+    private $version;
+    private $counter;
+    private $upDownCounter;
+    private $valueRecorder;
+
+    public function __construct(string $name, ?string $version = null)
+    {
+        $this->name = $name;
+        $this->version = $version !== null ? $version : '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVersion(): ?string
+    {
+        return $this->version;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function newCounter(string $name, string $description = null): API\Counter
+    {
+        $this->counter = new Counter($name, $description);
+
+        return $this->counter;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function newUpDownCounter(string $name, string $description = null): API\UpDownCounter
+    {
+        $this->upDownCounter = new UpDownCounter($name, $description);
+
+        return $this->upDownCounter;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function newValueRecorder(string $name, string $description = null): API\ValueRecorder
+    {
+        $this->valueRecorder = new ValueRecorder($name, $description);
+
+        return $this->valueRecorder;
+    }
 }
