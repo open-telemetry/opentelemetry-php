3898e0e964e32e2bce6ad18097d4d4d3e32e1a98
Shovnik Bhattacharya <shovnik@amazon.com>
2020-09-21 19:32:42 -0400

    First attempt at implementing probability sampler based on TraceId

--- sdk/Trace/Sampler/TraceIdRatioBasedSampler.php
+++ sdk/Trace/Sampler/TraceIdRatioBasedSampler.php
@@ -29,6 +29,9 @@ class TraceIdRatioBasedSampler implements Sampler
      */
     public function __construct(float $probability)
     {
+        if ($probability < 0.0 or $probability > 0.0) {
+            throw new InvalidArgumentException("probability should be be between 0.0 and 1.0.");
+        }
         $this->probability = $probability;
     }
 
@@ -50,7 +53,12 @@ class TraceIdRatioBasedSampler implements Sampler
         }
 
         // TODO: implement as a function of TraceID when specification is ready
-        $decision = (lcg_value() < $this->probability)
+        /**
+         * For compatibility with 64 bit IDs, the sampler checks the 64 first bits of the trace ID to decide whether to sample
+         */
+        $traceIdLimit = (1 << 64) - 1;
+        $traceIdLowerBytes = ($traceId[0]<<56) + ($traceId[1]<<48) + ($traceId[2]<<40) + ($traceId[3]<<32) + ($traceId[4]<<24) + ($traceId[5]<<16) + ($traceId[6]<<8) + $traceId[7]; 
+        $decision = ($traceIdLowerBytes < $probability * tradeIdLimit)
             ? SamplingResult::RECORD_AND_SAMPLED
             : SamplingResult::NOT_RECORD;
 
