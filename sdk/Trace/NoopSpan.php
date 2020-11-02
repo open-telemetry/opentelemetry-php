d1c19931db77b7a4141bbe049fe6a0ebf5a9ecbf
Amber Sistla <asistla@newrelic.com>
2020-11-02 00:55:12 -0800

    Ubuntu.

--- sdk/Trace/NoopSpan.php
+++ sdk/Trace/NoopSpan.php
@@ -36,7 +36,7 @@ class NoopSpan implements \OpenTelemetry\Trace\Span
     public function __construct(API\SpanContext $spanContext = null)
     {
         if (null == $spanContext) {
-            $this->context = new API\SpanContext(
+            $this->context = new SpanContext(
                 SpanContext::INVALID_TRACE,
                 SpanContext::INVALID_SPAN,
                 0,
