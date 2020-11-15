09a405a59c663c437b142f9b37728ded9e2931a6
Ritick Gautam <riticksinghrajput@gmail.com>
2020-11-15 13:18:36 +0530

    Bug fixed

--- script/proto_generate.sh
+++ script/proto_generate.sh
@@ -1,19 +1,31 @@
-git --version 2>&1 >/dev/null
-GIT_IS_AVAILABLE=$?
-if [ $GIT_IS_AVAILABLE -eq 0 ]
+GIT_IS_AVAILABLE=$(git --version 2>&1 >/dev/null)
+PROTOC_IS_AVAILABLE=$(protoc --version 2>&1 >/dev/null)
+if  $GIT_IS_AVAILABLE && $PROTOC_IS_AVAILABLE 
 then
-	cd ..
-	git clone https://github.com/open-telemetry/opentelemetry-proto
-	echo "Creating proto folder ..."
-	mkdir proto
-	protoc --version 3>&1 >/dev/null
-	PROTO_IS_AVAILABLE=$?
-	if [ $PROTO_IS_AVAILABLE -eq 0 ]
-	then
-		protoc --proto_path=opentelemetry-proto/ --php_out=proto $(find opentelemetry-proto/opentelemetry -iname "*.proto")
+	cd $(git rev-parse --show-toplevel)
+	if [ -d "./opentelemetry-proto" ]; then
+		echo "updating opentelemetry-proto"
+		cd opentelemetry-proto
+		git pull
+		cd ..
 	else
-		echo "protoc not available"
+		git clone https://github.com/open-telemetry/opentelemetry-proto
 	fi
+	if [ -d "./grpc" ]; then
+		rm -rf grpc
+	fi
+	git clone -b v1.33.x https://github.com/grpc/grpc
+	cd grpc
+	git submodule update --init
+	make grpc_php_plugin
+	cd ..
+	if [ -d "./proto" ]; then
+		echo "removing proto folder..."
+		rm -r proto
+	fi
+	mkdir proto
+	protoc --proto_path=opentelemetry-proto/ --php_out=proto --grpc_out=proto --plugin=protoc-gen-grpc=grpc/bins/opt/grpc_php_plugin $(find opentelemetry-proto/opentelemetry -iname "*.proto")
+	echo "proto folder created."
 else
-	echo "git not available"
+	echo "git or protoc not available"
 fi
