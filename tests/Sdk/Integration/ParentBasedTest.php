00ff4e8f90be7462b4290a32c7ba9547ca212512
lalex <github@lalex.nsk.ru>
2021-01-11 16:12:05 +0200

    Update ParentBased sampler according to specs. (#234)

--- tests/Sdk/Integration/ParentBasedTest.php
+++ tests/Sdk/Integration/ParentBasedTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\Tests\Sdk\Integration;
 
+use OpenTelemetry\Sdk\Trace\Sampler;
 use OpenTelemetry\Sdk\Trace\Sampler\ParentBased;
 use OpenTelemetry\Sdk\Trace\SamplingResult;
 use OpenTelemetry\Sdk\Trace\SpanContext;
@@ -12,58 +13,103 @@ use PHPUnit\Framework\TestCase;
 
 class ParentBasedTest extends TestCase
 {
-    public function testRecordParentBasedDecision()
+    /**
+     * @test
+     */
+    public function testParentBasedRootSpan()
     {
-        $parentContext = new SpanContext(
-            '4bf92f3577b34da6a3ce929d0e0e4736',
-            '00f067aa0ba902b7',
-            0x1
-        );
-        $sampler = new ParentBased();
-        $decision = $sampler->shouldSample(
-            $parentContext,
-            '4bf92f3577b34da6a3ce929d0e0e4736',
-            '00f067aa0ba902b7',
-            'test.opentelemetry.io',
-            API\SpanKind::KIND_INTERNAL
-        );
-        $this->assertEquals(SamplingResult::RECORD_AND_SAMPLED, $decision->getDecision());
-    }
+        $rootSampler = $this->createMockSamplerInvokedOnce(SamplingResult::RECORD_AND_SAMPLED);
 
-    public function testSkipParentBasedDecision()
-    {
-        $parentContext = new SpanContext(
-            '4bf92f3577b34da6a3ce929d0e0e4736',
-            '00f067aa0ba902b7',
-            0
-        );
-        $sampler = new ParentBased();
-        $decision = $sampler->shouldSample(
-            $parentContext,
-            '4bf92f3577b34da6a3ce929d0e0e4736',
-            '00f067aa0ba902b7',
-            'test.opentelemetry.io',
-            API\SpanKind::KIND_INTERNAL
-        );
-        $this->assertEquals(SamplingResult::NOT_RECORD, $decision->getDecision());
-    }
-
-    public function testNullParentBasedDecision()
-    {
-        $sampler = new ParentBased();
-        $decision = $sampler->shouldSample(
+        $sampler = new ParentBased($rootSampler);
+        $sampler->shouldSample(
             null,
             '4bf92f3577b34da6a3ce929d0e0e4736',
             '00f067aa0ba902b7',
             'test.opentelemetry.io',
             API\SpanKind::KIND_INTERNAL
         );
-        $this->assertEquals(SamplingResult::NOT_RECORD, $decision->getDecision());
     }
 
+    /**
+     * @dataProvider parentContextData
+     */
+    public function testParentBased(
+        $parentContext,
+        ?Sampler $remoteParentSampled = null,
+        ?Sampler $remoteParentNotSampled = null,
+        ?Sampler $localParentSampled = null,
+        ?Sampler $localParentNotSampled = null,
+        $expectedDdecision
+    ) {
+        $rootSampler = $this->createMockSamplerNeverInvoked();
+
+        $sampler = new ParentBased($rootSampler, $remoteParentSampled, $remoteParentNotSampled, $localParentSampled, $localParentNotSampled);
+        $decision = $sampler->shouldSample(
+            $parentContext,
+            '4bf92f3577b34da6a3ce929d0e0e4736',
+            '00f067aa0ba902b7',
+            'test.opentelemetry.io',
+            API\SpanKind::KIND_INTERNAL
+        );
+        $this->assertEquals($expectedDdecision, $decision->getDecision());
+    }
+
+    public function parentContextData(): array
+    {
+        return [
+            // remote, sampled, default sampler
+            [$this->createParentContext(true, true), null, null, null, null, SamplingResult::RECORD_AND_SAMPLED],
+            // remote, not sampled, default sampler
+            [$this->createParentContext(false, true), null, null, null, null, SamplingResult::NOT_RECORD],
+            // local, sampled, default sampler
+            [$this->createParentContext(true, false), null, null, null, null, SamplingResult::RECORD_AND_SAMPLED],
+            // local, not sampled, default sampler
+            [$this->createParentContext(false, false), null, null, null, null, SamplingResult::NOT_RECORD],
+            // remote, sampled
+            [$this->createParentContext(true, true), $this->createMockSamplerInvokedOnce(SamplingResult::RECORD_AND_SAMPLED), null, null, null, SamplingResult::RECORD_AND_SAMPLED],
+            // remote, not sampled
+            [$this->createParentContext(false, true), null, $this->createMockSamplerInvokedOnce(SamplingResult::NOT_RECORD), null, null, SamplingResult::NOT_RECORD],
+            // local, sampled
+            [$this->createParentContext(true, false), null, null, $this->createMockSamplerInvokedOnce(SamplingResult::RECORD_AND_SAMPLED), null, SamplingResult::RECORD_AND_SAMPLED],
+            // local, not sampled
+            [$this->createParentContext(false, false), null, null, null, $this->createMockSamplerInvokedOnce(SamplingResult::NOT_RECORD), SamplingResult::NOT_RECORD],
+        ];
+    }
+
+    /**
+     * @test
+     */
     public function testParentBasedDescription()
     {
-        $sampler = new ParentBased();
+        $rootSampler = self::createMock(Sampler::class);
+        $sampler = new ParentBased($rootSampler);
         $this->assertEquals('ParentBased', $sampler->getDescription());
+    }
+
+    private function createParentContext(bool $sampled, bool $isRemote): SpanContext
+    {
+        return SpanContext::restore(
+            '4bf92f3577b34da6a3ce929d0e0e4736',
+            '00f067aa0ba902b7',
+            $sampled,
+            $isRemote
+        );
+    }
+
+    private function createMockSamplerNeverInvoked(): Sampler
+    {
+        $sampler = self::createMock(Sampler::class);
+        $sampler->expects($this->never())->method('shouldSample');
+
+        return $sampler;
+    }
+
+    private function createMockSamplerInvokedOnce(int $resultDecision): Sampler
+    {
+        $sampler = self::createMock(Sampler::class);
+        $sampler->expects($this->once())->method('shouldSample')
+            ->willReturn(new SamplingResult($resultDecision));
+
+        return $sampler;
     }
 }
