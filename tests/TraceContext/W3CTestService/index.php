7ec3d412ba2e76d23c9e62e4e4a63623c73adaed
Fahmy Mohammed <fmohammed@newrelic.com>
2021-04-07 10:40:13 -0700

    more cleanup

--- tests/TraceContext/W3CTestService/index.php
+++ tests/TraceContext/W3CTestService/index.php
@@ -13,7 +13,6 @@ use OpenTelemetry\Sdk\Trace\SpanProcessor\BatchSpanProcessor;
 use OpenTelemetry\Sdk\Trace\TracerProvider;
 use OpenTelemetry\Trace as API;
 use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
