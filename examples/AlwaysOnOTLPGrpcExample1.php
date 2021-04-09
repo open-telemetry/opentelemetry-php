181d63aef557be22d1dd9ae25df464e4ae5947c0
Amber Sistla <asistla@newrelic.com>
2021-04-09 14:55:15 -0700

    Two different ways to make children spans.

---
+++ examples/AlwaysOnOTLPGrpcExample1.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+require __DIR__ . '/../vendor/autoload.php';
+
+use OpenTelemetry\Contrib\OtlpGrpc\Exporter as OTLPExporter;
+use OpenTelemetry\Sdk\Trace\Attributes;
+use OpenTelemetry\Sdk\Trace\Clock;
+use OpenTelemetry\Sdk\Trace\Sampler\AlwaysOnSampler;
+use OpenTelemetry\Sdk\Trace\SamplingResult;
+use OpenTelemetry\Sdk\Trace\SpanProcessor\SimpleSpanProcessor;
+use OpenTelemetry\Sdk\Trace\TracerProvider;
+use OpenTelemetry\Context\Context;
+use OpenTelemetry\Trace as API;
+
+$sampler = new AlwaysOnSampler();
+$samplingResult = $sampler->shouldSample(
+    Context::getCurrent(),
+    md5((string) microtime(true)),
+    'io.opentelemetry.example',
+    API\SpanKind::KIND_INTERNAL
+);
+
+$Exporter = new OTLPExporter(
+    'OTLP Grpc Example Service'
+);
+
+if (SamplingResult::RECORD_AND_SAMPLE === $samplingResult->getDecision()) {
+    echo 'Starting OTLPGrpcExample';
+    $tracer = (new TracerProvider())
+        ->addSpanProcessor(new SimpleSpanProcessor($Exporter))
+        ->getTracer('io.opentelemetry.contrib.php');
+
+    for ($i = 0; $i < 5; $i++) {
+        // start a span, register some events
+        $timestamp = Clock::get()->timestamp();
+        $span = $tracer->startAndActivateSpan('session.generate.span.' . microtime(true));
+
+        $childSpan = $tracer->startAndActivateSpanFromContext('child', $span->getContext());
+
+        // temporarily setting service name here.  It should eventually be pulled from tracer.resources.
+        $span->setAttribute('service.name', 'alwaysOnOTLPGrpcExample');
+
+        $span->setAttribute('remote_ip', '1.2.3.4')
+            ->setAttribute('country', 'USA');
+
+        $span->addEvent('found_login' . $i, $timestamp, new Attributes([
+            'id' => $i,
+            'username' => 'otuser' . $i,
+        ]));
+        $span->addEvent('generated_session', $timestamp, new Attributes([
+            'id' => md5((string) microtime(true)),
+        ]));
+
+
+        // temporarily setting service name here.  It should eventually be pulled from tracer.resources.
+        $childSpan->setAttribute('service.name', 'alwaysOnOTLPGrpcExample');
+
+        $childSpan->setAttribute('attr_one', 'one')
+            ->setAttribute('attr_two', 'two');
+
+        $childSpan->addEvent('found_event1' . $i, $timestamp, new Attributes([
+            'id' => $i,
+            'username' => 'child' . $i,
+        ]));
+
+        $tracer->endActiveSpan();
+        $tracer->setActiveSpan($span);
+        $tracer->endActiveSpan();
+    }
+    echo PHP_EOL . 'OTLPGrpcExample complete!  ';
+} else {
+    echo PHP_EOL . 'OTLPGrpcExample tracing is not enabled';
+}
+
+echo PHP_EOL;
