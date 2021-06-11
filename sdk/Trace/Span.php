8d7d3889143b29747cbebda144073d26a35ff720
Tobias Bachert <tobiasbachert@horn-gmbh.com>
2021-06-11 19:11:24 +0200

    Add attributes parameter to ::recordException()

--- sdk/Trace/Span.php
+++ sdk/Trace/Span.php
@@ -219,18 +219,21 @@ class Span implements API\Span
         return $this;
     }
 
-    public function recordException(Exception $exception): API\Span
+    public function recordException(Exception $exception, ?API\Attributes $attributes = null): API\Span
     {
-        $attributes = new Attributes(
+        $eventAttributes = new Attributes(
             [
                 'exception.type' => get_class($exception),
                 'exception.message' => $exception->getMessage(),
                 'exception.stacktrace' => $exception->getTraceAsString(),
             ]
         );
+        foreach ($attributes ?? [] as $attribute) {
+            $eventAttributes->setAttribute($attribute->getKey(), $attribute->getValue());
+        }
         $timestamp = time();
 
-        return  $this->addEvent('exception', $timestamp, $attributes);
+        return $this->addEvent('exception', $timestamp, $eventAttributes);
     }
 
     public function getEvents(): API\Events
