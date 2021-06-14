e2d872539daae2178c06b876ffc2e8e0c14bfa4f
Tobias Bachert <tobiasbachert@horn-gmbh.com>
2021-06-14 14:22:57 +0200

    Propagate ::startSpan() attributes to created span (#353)

--- tests/Sdk/Unit/Trace/TracerTest.php
+++ tests/Sdk/Unit/Trace/TracerTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace OpenTelemetry\Tests\Sdk\Unit\Trace;
 
 use OpenTelemetry\Context\Context;
+use OpenTelemetry\Sdk\Trace\Attributes;
 use OpenTelemetry\Sdk\Trace\NoopSpan;
 use OpenTelemetry\Sdk\Trace\Span;
 use OpenTelemetry\Sdk\Trace\SpanContext;
 use OpenTelemetry\Sdk\Trace\SpanProcessor;
 use OpenTelemetry\Sdk\Trace\TracerProvider;
+use OpenTelemetry\Trace as API;
 use PHPUnit\Framework\TestCase;
 
 class TracerTest extends TestCase
@@ -161,5 +163,24 @@ class TracerTest extends TestCase
         $span = $tracer->startSpan('test.span');
         $tracer->setActiveSpan($span);
         $tracer->endActiveSpan();
+    }
+
+    /**
+     * @test
+     */
+    public function startSpanAttributesShouldBePropagatedToSpan()
+    {
+        $tracerProvider = new TracerProvider();
+        $tracer = $tracerProvider->getTracer('OpenTelemetry.TracerTest');
+
+        $attributes = [
+            'attribute-1' => 'value-1',
+            'attribute-2' => 'value-2',
+        ];
+        $span = $tracer->startSpan('test.span', null, API\SpanKind::KIND_INTERNAL, new Attributes($attributes));
+
+        $this->assertSame($attributes, array_map(function (API\Attribute $attribute) {
+            return $attribute->getValue();
+        }, iterator_to_array($span->getAttributes())));
     }
 }
