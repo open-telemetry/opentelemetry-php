3e7e666dff9cf4295e756fe066a6e7389afd9018
Timo Michna <timomichna@yahoo.de>
2021-11-04 08:30:28 +0100

    Add FriendlySpanConverter

---
+++ src/SDK/Trace/SpanExporter/FriendlySpanConverter.php
@@ -0,0 +1,175 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\SDK\Trace\SpanExporter;
+
+use OpenTelemetry\API\Trace\AttributesInterface;
+use OpenTelemetry\API\Trace\EventInterface;
+use OpenTelemetry\API\Trace\LinkInterface;
+use OpenTelemetry\API\Trace\SpanContextInterface;
+use OpenTelemetry\API\Trace\SpanKind;
+use OpenTelemetry\SDK\Resource\ResourceInfo;
+use OpenTelemetry\SDK\Trace\SpanConverterInterface;
+use OpenTelemetry\SDK\Trace\SpanDataInterface;
+use OpenTelemetry\SDK\Trace\StatusDataInterface;
+use ReflectionClass;
+
+class FriendlySpanConverter implements SpanConverterInterface
+{
+    private const NAME_ATTR = 'name';
+    private const CONTEXT_ATTR = 'context';
+    private const TRACE_ID_ATTR = 'trace_id';
+    private const SPAN_ID_ATTR = 'span_id';
+    private const TRACE_STATE_ATTR = 'trace_state';
+    private const RESOURCE_ATTR = 'resource';
+    private const PARENT_SPAN_ATTR = 'parent_span_id';
+    private const KIND_ATTR = 'kind';
+    private const START_ATTR = 'start';
+    private const END_ATTR = 'end';
+    private const ATTRIBUTES_ATTR = 'attributes';
+    private const STATUS_ATTR = 'status';
+    private const CODE_ATTR = 'code';
+    private const DESCRIPTION_ATTR = 'description';
+    private const EVENTS_ATTR = 'events';
+    private const TIMESTAMP_ATTR = 'timestamp';
+    private const LINKS_ATTR = 'links';
+
+    /**
+     * friendlySpan does the heavy lifting converting a span into an array
+     *
+     * @param SpanDataInterface $span
+     * @return array
+     */
+    public function convert(SpanDataInterface $span): array
+    {
+        return [
+            self::NAME_ATTR => $span->getName(),
+            self::CONTEXT_ATTR => $this->convertContext($span->getContext()),
+            self::RESOURCE_ATTR => $this->convertResource($span->getResource()),
+            self::PARENT_SPAN_ATTR => $this->covertParentContext($span->getParentContext()),
+            self::KIND_ATTR => $this->convertKind($span->getKind()),
+            self::START_ATTR => $span->getStartEpochNanos(),
+            self::END_ATTR => $span->getEndEpochNanos(),
+            self::ATTRIBUTES_ATTR => $this->convertAttributes($span->getAttributes()),
+            self::STATUS_ATTR => $this->covertStatus($span->getStatus()),
+            self::EVENTS_ATTR => $this->convertEvents($span->getEvents()),
+            self::LINKS_ATTR => $this->convertLinks($span->getLinks()),
+        ];
+    }
+
+    /**
+     * @param SpanContextInterface $context
+     * @return array
+     */
+    private function convertContext(SpanContextInterface $context): array
+    {
+        return [
+            self::TRACE_ID_ATTR => $context->getTraceId(),
+            self::SPAN_ID_ATTR => $context->getSpanId(),
+            self::TRACE_STATE_ATTR => (string) $context->getTraceState(),
+        ];
+    }
+
+    /**
+     * @param ResourceInfo $resource
+     * @return array
+     */
+    private function convertResource(ResourceInfo $resource): array
+    {
+        $tmp = [];
+
+        foreach ($resource->getAttributes() as $attribute) {
+            $tmp[$attribute->getKey()] = $attribute->getValue();
+        }
+
+        return $tmp;
+    }
+
+    /**
+     * @param SpanContextInterface $context
+     * @return string
+     */
+    private function covertParentContext(SpanContextInterface $context): string
+    {
+        return $context->isValid() ? $context->getSpanId() : '';
+    }
+
+    /**
+     * Translates SpanKind from its integer representation to a more human friendly string.
+     *
+     * @param int $kind
+     * @return string
+     */
+    private function convertKind(int $kind): string
+    {
+        return  array_flip(
+            (new ReflectionClass(SpanKind::class))
+                ->getConstants()
+        )[$kind];
+    }
+
+    /**
+     * @param AttributesInterface $attributes
+     * @return array
+     */
+    private function convertAttributes(AttributesInterface $attributes): array
+    {
+        $tmp = [];
+
+        foreach ($attributes as $attribute) {
+            $tmp[$attribute->getKey()] = $attribute->getValue();
+        }
+
+        return $tmp;
+    }
+
+    /**
+     * @param StatusDataInterface $status
+     * @return array
+     */
+    private function covertStatus(StatusDataInterface $status): array
+    {
+        return [
+            self::CODE_ATTR => $status->getCode(),
+            self::DESCRIPTION_ATTR => $status->getDescription(),
+        ];
+    }
+
+    /**
+     * @param array<EventInterface> $events
+     * @return array
+     */
+    private function convertEvents(array $events): array
+    {
+        $result = [];
+
+        foreach ($events as $event) {
+            $result[] = [
+                self::NAME_ATTR => $event->getName(),
+                self::TIMESTAMP_ATTR => $event->getEpochNanos(),
+                self::ATTRIBUTES_ATTR => $this->convertAttributes($event->getAttributes()),
+            ];
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array<LinkInterface> $links
+     * @return array
+     */
+    private function convertLinks(array $links): array
+    {
+        $result = [];
+
+        foreach ($links as $link) {
+            $result[] = [
+                self::CONTEXT_ATTR => $this->convertContext($link->getSpanContext()),
+                self::ATTRIBUTES_ATTR => $this->convertAttributes($link->getAttributes()),
+            ];
+        }
+
+        return $result;
+    }
+}
---
+++ tests/SDK/Unit/Trace/SpanExporter/FriendlySpanConverterTest.php
@@ -0,0 +1,309 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\Tests\SDK\Unit\Trace\SpanExporter;
+
+use ArrayIterator;
+use OpenTelemetry\API\Trace\AttributeInterface;
+use OpenTelemetry\API\Trace\AttributesInterface;
+use OpenTelemetry\API\Trace\AttributesIteratorInterface;
+use OpenTelemetry\API\Trace\EventInterface;
+use OpenTelemetry\API\Trace\LinkInterface;
+use OpenTelemetry\API\Trace\SpanContextInterface;
+use OpenTelemetry\API\Trace\SpanKind;
+use OpenTelemetry\API\Trace\TraceStateInterface;
+use OpenTelemetry\SDK\Resource\ResourceInfo;
+use OpenTelemetry\SDK\Trace\Attribute;
+use OpenTelemetry\SDK\Trace\SpanDataInterface;
+use OpenTelemetry\SDK\Trace\SpanExporter\FriendlySpanConverter;
+use OpenTelemetry\SDK\Trace\StatusDataInterface;
+use PHPUnit\Framework\TestCase;
+
+//use DG\BypassFinals;
+
+class FriendlySpanConverterTest extends TestCase
+{
+    private const TEST_DATA = [
+        'name' => 'my.service',
+        'parent_span_id' => '1000000000000000',
+        'kind' => 'KIND_INTERNAL',
+        'start' => 1505855794194009601,
+        'end' => 1505855799465726528,
+        'context' => [
+            'trace_id' => '00000000000000000000000000000000',
+            'span_id' => '0000000000000000',
+            'trace_state' => 'foz=baz,foo=bar',
+        ],
+        'resource' => [
+            'telemetry.sdk.name' => 'opentelemetry',
+            'telemetry.sdk.language' => 'php',
+            'telemetry.sdk.version' => 'dev',
+        ],
+        'attributes' => [
+            'fruit' => 'apple',
+        ],
+        'status' => [
+            'code' => 'Error',
+            'description' => 'status_description',
+        ],
+        'events' => [[
+            'name' => 'validators.list',
+            'timestamp' => 1505855799433901068,
+            'attributes' => [
+                'job' => 'stage.updateTime',
+            ],
+        ],[
+            'name' => 'user.created',
+            'timestamp' => 1505855799433901555,
+            'attributes' => [
+                'name' => 'John Doe',
+            ],
+        ],],
+        'links' => [[
+            'context' => [
+                'trace_id' => '20000000000000000000000000000000',
+                'span_id' => '2000000000000000',
+                'trace_state' => 'foo=baz,foz=bar',
+            ],
+            'attributes' => [
+                'foo' => 'bar',
+            ], ],[
+            'context' => [
+                'trace_id' => '20000000000000000000000000000000',
+                'span_id' => '3000000000000000',
+                'trace_state' => 'baz=foz,bar=foo',
+            ],
+            'attributes' => [
+                'foz' => 'baz',
+            ], ],
+        ],
+    ];
+
+    public function testConvert(): void
+    {
+        $this->assertEquals(
+            self::TEST_DATA,
+            (new FriendlySpanConverter())->convert(
+                $this->createSpanDataInterfaceMock()
+            )
+        );
+    }
+
+    private function createSpanDataInterfaceMock(): SpanDataInterface
+    {
+        $mock = $this->getMockBuilder(SpanDataInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mock->method('getName')
+            ->willReturn(self::TEST_DATA['name']);
+
+        $mock->method('getContext')
+            ->willReturn(
+                $this->createSpanContextMock(
+                    self::TEST_DATA['context']['span_id'],
+                    self::TEST_DATA['context']['trace_id'],
+                    self::TEST_DATA['context']['trace_state'],
+                )
+            );
+
+        $mock->method('getParentContext')
+            ->willReturn(
+                $this->createSpanContextMock(
+                    self::TEST_DATA['parent_span_id'],
+                )
+            );
+
+        $mock->method('getResource')
+            ->willReturn(
+                $this->createResourceInfoMock()
+            );
+
+        $mock->method('getKind')
+            ->willReturn(SpanKind::KIND_INTERNAL);
+
+        $mock->method('getStartEpochNanos')
+            ->willReturn(self::TEST_DATA['start']);
+
+        $mock->method('getEndEpochNanos')
+            ->willReturn(self::TEST_DATA['end']);
+
+        $mock->method('getAttributes')
+            ->willReturn($this->createAttributesInterfaceMock(self::TEST_DATA['attributes']));
+
+        $mock->method('getStatus')
+            ->willReturn($this->createStatusDataMock());
+
+        $events = [];
+        foreach (self::TEST_DATA['events'] as $event) {
+            $events[] = $this->createEventInterfaceMock(
+                $event['name'],
+                $event['timestamp'],
+                $this->createAttributesInterfaceMock(
+                    $event['attributes']
+                )
+            );
+        }
+        $mock->method('getEvents')->willReturn($events);
+
+        $links = [];
+        foreach (self::TEST_DATA['links'] as $link) {
+            $links[] = $this->createLinkInterfaceMock(
+                $this->createSpanContextMock(
+                    $link['context']['span_id'],
+                    $link['context']['trace_id'],
+                    $link['context']['trace_state']
+                ),
+                $this->createAttributesInterfaceMock($link['attributes'])
+            );
+        }
+        $mock->method('getLinks')->willReturn($links);
+
+        return $mock;
+    }
+
+    private function createSpanContextMock(string $spanId, string $traceId = '0', string $traceState = null): SpanContextInterface
+    {
+        $mock = $this->createMock(SpanContextInterface::class);
+
+        $mock->method('getSpanId')
+            ->willReturn($spanId);
+
+        $mock->method('getTraceId')
+            ->willReturn($traceId);
+
+        $mock->method('getTraceState')
+            ->willReturn(
+                $traceState === null ? null : $this->createTraceStateInterfaceMock($traceState)
+            );
+
+        $mock->method('isValid')
+            ->willReturn(true);
+
+        return $mock;
+    }
+
+    private function createTraceStateInterfaceMock(string $traceState): TraceStateInterface
+    {
+        $mock = $this->createMock(TraceStateInterface::class);
+
+        $mock->method('__toString')
+            ->willReturn($traceState);
+
+        return $mock;
+    }
+
+    private function createResourceInfoMock(): ResourceInfo
+    {
+        $mock = $this->getMockBuilder(ResourceInfo::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mock->method('getAttributes')
+            ->willReturn($this->createAttributesInterfaceMock(self::TEST_DATA['resource']));
+
+        return $mock;
+    }
+
+    private function createStatusDataMock(): StatusDataInterface
+    {
+        $mock = $this->createMock(StatusDataInterface::class);
+
+        $mock->method('getCode')
+            ->willReturn(self::TEST_DATA['status']['code']);
+
+        $mock->method('getDescription')
+            ->willReturn(self::TEST_DATA['status']['description']);
+
+        return $mock;
+    }
+
+    private function createAttributesInterfaceMock(array $items): AttributesInterface
+    {
+        $mock = $this->createMock(AttributesInterface::class);
+
+        $attributes = [];
+        foreach ($items as $key => $value) {
+            $attributes[$key] = $this->createAttributeMock($key, $value);
+        }
+        $mock->method('getIterator')
+        ->willReturn(
+            $this->createAttributesIterator($attributes)
+        );
+
+        return $mock;
+    }
+
+    private function createAttributeMock(string $key, $value): Attribute
+    {
+        $mock = $this->getMockBuilder(Attribute::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mock->method('getKey')
+            ->willReturn($key);
+
+        $mock->method('getValue')
+            ->willReturn($value);
+
+        return $mock;
+    }
+
+    public function createEventInterfaceMock(string $name, int $timestamp, AttributesInterface $attributes): EventInterface
+    {
+        $mock = $this->createMock(EventInterface::class);
+
+        $mock->method('getName')->willReturn($name);
+        $mock->method('getEpochNanos')->willReturn($timestamp);
+        $mock->method('getAttributes')->willReturn($attributes);
+
+        return $mock;
+    }
+
+    public function createLinkInterfaceMock(SpanContextInterface $context, AttributesInterface $attributes): LinkInterface
+    {
+        $mock = $this->createMock(LinkInterface::class);
+
+        $mock->method('getSpanContext')->willReturn($context);
+        $mock->method('getAttributes')->willReturn($attributes);
+
+        return $mock;
+    }
+
+    public function createAttributesIterator(array $items): AttributesIteratorInterface
+    {
+        return new class($items) implements AttributesIteratorInterface {
+            private ArrayIterator $iterator;
+            public function __construct($attributes)
+            {
+                $this->iterator = new ArrayIterator($attributes);
+            }
+
+            public function key(): string
+            {
+                return (string) $this->iterator->key();
+            }
+
+            public function current(): AttributeInterface
+            {
+                return $this->iterator->current();
+            }
+
+            public function rewind(): void
+            {
+                $this->iterator->rewind();
+            }
+
+            public function valid(): bool
+            {
+                return $this->iterator->valid();
+            }
+
+            public function next(): void
+            {
+                $this->iterator->next();
+            }
+        };
+    }
+}
