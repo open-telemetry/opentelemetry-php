b8597b5d8383218d1965bdeaf3b03879abeac867
Timo Michna <timomichna@yahoo.de>
2021-11-04 09:50:28 +0100

    Use FriendlySpanConverter

--- src/SDK/Trace/SpanExporter/ConsoleSpanExporter.php
+++ src/SDK/Trace/SpanExporter/ConsoleSpanExporter.php
@@ -4,137 +4,42 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\SDK\Trace\SpanExporter;
 
-use OpenTelemetry\API\Trace\SpanKind;
-use OpenTelemetry\SDK\Trace;
-use ReflectionClass;
+use OpenTelemetry\SDK\Trace\Behavior\SpanExporterTrait;
+use OpenTelemetry\SDK\Trace\Behavior\UsesSpanConverterTrait;
+use OpenTelemetry\SDK\Trace\SpanConverterInterface;
+use OpenTelemetry\SDK\Trace\SpanExporterInterface;
+use Throwable;
 
-class ConsoleSpanExporter implements Trace\SpanExporterInterface
+class ConsoleSpanExporter implements SpanExporterInterface
 {
-    private $running = true;
+    use SpanExporterTrait;
+    use UsesSpanConverterTrait;
+
+    public function __construct(?SpanConverterInterface $converter = null)
+    {
+        $this->setSpanConverter($converter ?? new FriendlySpanConverter());
+    }
 
     /** @inheritDoc */
     public function export(iterable $spans): int
     {
-        foreach ($spans as $span) {
-            print(json_encode($this->friendlySpan($span), JSON_PRETTY_PRINT) . PHP_EOL);
+        try {
+            foreach ($spans as $span) {
+                print(json_encode(
+                    $this->getSpanConverter()->convert($span),
+                    JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT
+                ) . PHP_EOL
+                );
+            }
+        } catch (Throwable $t) {
+            return SpanExporterInterface::STATUS_FAILED_NOT_RETRYABLE;
         }
 
-        return Trace\SpanExporterInterface::STATUS_SUCCESS;
-    }
-
-    /** @inheritDoc */
-    public function shutdown(): bool
-    {
-        $this->running = false;
-
-        return true;
-    }
-
-    /** @inheritDoc */
-    public function forceFlush(): bool
-    {
-        return true;
+        return SpanExporterInterface::STATUS_SUCCESS;
     }
 
     public static function fromConnectionString(string $endpointUrl = null, string $name = null, $args = null)
     {
-        return false;
-    }
-
-    /**
-     * @param iterable<\OpenTelemetry\API\Trace\EventInterface> $events
-     * @return array
-     */
-    private function friendlyEvents(iterable $events)
-    {
-        $tmp = [];
-
-        foreach ($events as $event) {
-            array_push($tmp, [
-                'name' => $event->getName(),
-                'timestamp' => $event->getEpochNanos(),
-                'attributes' => $this->friendlyAttributes($event->getAttributes()),
-            ]);
-        }
-
-        return $tmp;
-    }
-
-    /**
-     * @param \OpenTelemetry\API\Trace\AttributesInterface $attributes
-     * @return array
-     */
-    private function friendlyAttributes(\OpenTelemetry\API\Trace\AttributesInterface $attributes)
-    {
-        $tmp = [];
-
-        foreach ($attributes as $attribute) {
-            $tmp[$attribute->getKey()] = $attribute->getValue();
-        }
-
-        return $tmp;
-    }
-
-    /**
-     * Translates SpanKind from it's integer representation to a more human friendly string.
-     *
-     * @param int $kind
-     * @return string
-     */
-    private function friendlyKind(int $kind)
-    {
-        $spanKinds = (new ReflectionClass(SpanKind::class))->getConstants();
-
-        $kindSpans = array_flip($spanKinds);
-
-        return $kindSpans[$kind];
-    }
-
-    /**
-     * @param \OpenTelemetry\SDK\Resource\ResourceInfo $resource
-     * @return array
-     */
-    private function friendlyResource(\OpenTelemetry\SDK\Resource\ResourceInfo $resource): array
-    {
-        $tmp = [];
-
-        foreach ($resource->getAttributes()->getIterator() as $attribute) {
-            $tmp[$attribute->getKey()] = $attribute->getValue();
-        }
-
-        return $tmp;
-    }
-
-    /**
-     * friendlySpan does the heavy lifting converting a span into an array
-     *
-     * @param Trace\SpanDataInterface $span
-     * @return array
-     */
-    private function friendlySpan(Trace\SpanDataInterface $span)
-    {
-        $parent_span = $span->getParentContext();
-
-        $parent_span_id = $parent_span->isValid() ? $parent_span->getSpanId() : null;
-
-        return [
-            'name' => $span->getName(),
-            'context' => [
-                'trace_id' => $span->getContext()->getTraceId(),
-                'span_id' => $span->getContext()->getSpanId(),
-                'trace_state' => $span->getContext()->getTraceState(),
-            ],
-            'resource' => $this->friendlyResource($span->getResource()),
-            'parent_span_id' => $parent_span_id ? $parent_span_id : '',
-            'kind' => $this->friendlyKind($span->getKind()),
-            'start' => $span->getStartEpochNanos(),
-            'end' => $span->getEndEpochNanos(),
-            'attributes' => $this->friendlyAttributes($span->getAttributes()),
-            'status' => [
-                'code' => $span->getStatus()->getCode(),
-                'description' => $span->getStatus()->getDescription(),
-            ],
-            'events' => $this->friendlyEvents($span->getEvents()),
-        ];
+        return new self();
     }
 }
--- tests/SDK/Unit/Trace/SpanExporter/ConsoleSpanExporterTest.php
+++ tests/SDK/Unit/Trace/SpanExporter/ConsoleSpanExporterTest.php
@@ -4,92 +4,120 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\Tests\SDK\Unit\Trace\SpanExporter;
 
-use OpenTelemetry\API\Trace\StatusCode;
-use OpenTelemetry\SDK\InstrumentationLibrary;
-use OpenTelemetry\SDK\Resource\ResourceInfo;
-use OpenTelemetry\SDK\Trace\Attributes;
-use OpenTelemetry\SDK\Trace\SpanContext;
+use OpenTelemetry\SDK\Trace\SpanConverterInterface;
+use OpenTelemetry\SDK\Trace\SpanDataInterface;
 use OpenTelemetry\SDK\Trace\SpanExporter\ConsoleSpanExporter;
-use OpenTelemetry\SDK\Trace\StatusData;
-use OpenTelemetry\Tests\SDK\Util\SpanData;
+use OpenTelemetry\SDK\Trace\SpanExporterInterface;
 use PHPUnit\Framework\TestCase;
 
 class ConsoleSpanExporterTest extends TestCase
 {
-    public function testConsoleExporterWorks()
+    private const TEST_DATA = [
+        'name' => 'my.service',
+        'parent_span_id' => '1000000000000000',
+        'kind' => 'KIND_INTERNAL',
+        'start' => 1505855794194009601,
+        'end' => 1505855799465726528,
+        'context' => [
+            'trace_id' => '00000000000000000000000000000000',
+            'span_id' => '0000000000000000',
+            'trace_state' => 'foz=baz,foo=bar',
+        ],
+        'resource' => [
+            'telemetry.sdk.name' => 'opentelemetry',
+            'telemetry.sdk.language' => 'php',
+            'telemetry.sdk.version' => 'dev',
+        ],
+        'attributes' => [
+            'fruit' => 'apple',
+        ],
+        'status' => [
+            'code' => 'Error',
+            'description' => 'status_description',
+        ],
+        'events' => [[
+            'name' => 'validators.list',
+            'timestamp' => 1505855799433901068,
+            'attributes' => [
+                'job' => 'stage.updateTime',
+            ],
+        ],],
+    ];
+
+    public function testExportSuccess(): void
     {
-        $span = (new SpanData())
-        ->setName('my.service')
-        ->setParentContext(
-            SpanContext::create(
-                '10000000000000000000000000000000',
-                '1000000000000000'
-            )
-        )
-        ->setStatus(
-            new StatusData(
-                StatusCode::STATUS_ERROR,
-                'status_description'
-            )
-        )
-        ->setInstrumentationLibrary(new InstrumentationLibrary(
-            'instrumentation_library_name',
-            'instrumentation_library_version'
-        ))
-        ->addAttribute('fruit', 'apple')
-        ->setResource(ResourceInfo::defaultResource())
-        ->addEvent('validators.list', new Attributes(['job' => 'stage.updateTime']), 1505855799433901068)
-        ->setHasEnded(true);
+        $converter = $this->createMock(SpanConverterInterface::class);
+        $converter->expects($this->once())
+            ->method('convert')
+            ->willReturn(self::TEST_DATA);
 
-        $exp = new ConsoleSpanExporter();
-        $exp->export([$span]);
+        ob_start();
 
-        $expected = <<<EOF
-        {
-            "name": "my.service",
-            "context": {
-                "trace_id": "00000000000000000000000000000000",
-                "span_id": "0000000000000000",
-                "trace_state": null
-            },
-            "resource": {
-                "telemetry.sdk.name": "opentelemetry",
-                "telemetry.sdk.language": "php",
-                "telemetry.sdk.version": "dev"
-            },
-            "parent_span_id": "1000000000000000",
-            "kind": "KIND_INTERNAL",
-            "start": 1505855794194009601,
-            "end": 1505855799465726528,
-            "attributes": {
-                "fruit": "apple"
-            },
-            "status": {
-                "code": "Error",
-                "description": "status_description"
-            },
-            "events": [
-                {
-                    "name": "validators.list",
-                    "timestamp": 1505855799433901068,
-                    "attributes": {
-                        "job": "stage.updateTime"
-                    }
-                }
-            ]
-        }
+        $this->assertSame(
+            SpanExporterInterface::STATUS_SUCCESS,
+            (new ConsoleSpanExporter($converter))->export([
+                $this->createMock(SpanDataInterface::class),
+            ])
+        );
 
-        EOF;
-
-        $this->expectOutputString($expected);
+        ob_end_clean();
     }
 
-    public function test_shutdown(): void
+    public function testExportFailed(): void
+    {
+        $resource = fopen('php://stdin', 'rb');
+        $converter = $this->createMock(SpanConverterInterface::class);
+        $converter->expects($this->once())
+            ->method('convert')
+            ->willReturn([$resource]);
+
+        ob_start();
+
+        $this->assertSame(
+            SpanExporterInterface::STATUS_FAILED_NOT_RETRYABLE,
+            (new ConsoleSpanExporter($converter))->export([
+                $this->createMock(SpanDataInterface::class),
+            ])
+        );
+
+        ob_end_clean();
+        fclose($resource);
+    }
+
+    public function testExportOutput(): void
+    {
+        try {
+            $expected = json_encode(self::TEST_DATA, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT) . PHP_EOL;
+        } catch (\Throwable $t) {
+            $this->fail($t->getMessage());
+        }
+
+        $converter = $this->createMock(SpanConverterInterface::class);
+        $converter->expects($this->once())
+            ->method('convert')
+            ->willReturn(self::TEST_DATA);
+
+        $this->expectOutputString($expected);
+
+        (new ConsoleSpanExporter($converter))->export([
+            $this->createMock(SpanDataInterface::class),
+        ]);
+    }
+
+    public function testFromConnectionString(): void
+    {
+        $this->assertInstanceOf(
+            ConsoleSpanExporter::class,
+            ConsoleSpanExporter::fromConnectionString()
+        );
+    }
+
+    public function testShutdown(): void
     {
         $this->assertTrue((new ConsoleSpanExporter())->shutdown());
     }
 
-    public function test_forceFlush(): void
+    public function testForceFlush(): void
     {
         $this->assertTrue((new ConsoleSpanExporter())->forceFlush());
     }
