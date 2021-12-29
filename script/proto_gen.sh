7a2cacc54ee798b3ec001d063461c7bf36e19d39
Timo Michna <timomichna@yahoo.de>
2021-12-29 20:17:10 +0100

    Move otel protobuf files to 'otel' subdirectory

--- script/proto_gen.sh
+++ script/proto_gen.sh
@@ -1,6 +1,31 @@
 apk update && apk add git
 
-mkdir /mnt/proto
-git clone https://github.com/open-telemetry/opentelemetry-proto
+DESTINATION_DIR=/mnt/proto/otel
+REPO_DIR=opentelemetry-proto
 
-protoc --proto_path=opentelemetry-proto/ --php_out=/mnt/proto --grpc_out=/mnt/proto --plugin=protoc-gen-grpc=usr/local/bin/grpc_php_plugin $(find opentelemetry-proto/opentelemetry -iname "*.proto")
+mkdir -p $DESTINATION_DIR
+rm -R ./$REPO_DIR
+git clone https://github.com/open-telemetry/$REPO_DIR
+
+TAG=$(
+    # shellcheck disable=SC2164
+    cd ./$REPO_DIR
+
+    # shellcheck disable=SC2046
+    # shellcheck disable=SC2006
+    TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+    git checkout "${TAG}"
+
+    echo "$TAG"
+)
+
+echo "Generating protobuf files for version $TAG ..."
+
+# shellcheck disable=SC2046
+protoc --proto_path=$REPO_DIR/ --php_out=$DESTINATION_DIR --grpc_out=$DESTINATION_DIR \
+ --plugin=protoc-gen-grpc=usr/local/bin/grpc_php_plugin $(find $REPO_DIR/opentelemetry/proto -iname "*.proto")
+
+echo "$TAG" > $DESTINATION_DIR/VERSION
+
+echo "Done!"
