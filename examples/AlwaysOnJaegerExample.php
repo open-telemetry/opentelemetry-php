c7f3361bdc03cf9442a9bf6ea7aaf363323d3ac5
Brett McBride <brett.a.mcbride@gmail.com>
2022-01-06 21:56:57 +1100

    fixing jaeger example (#541)
    * fixing jaeger example
    
    * tidy

--- examples/AlwaysOnJaegerExample.php
+++ examples/AlwaysOnJaegerExample.php
@@ -3,75 +3,56 @@
 declare(strict_types=1);
 require __DIR__ . '/../vendor/autoload.php';
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\HttpFactory;
-use OpenTelemetry\API\Trace as API;
-use OpenTelemetry\Context\Context;
 use OpenTelemetry\Contrib\Jaeger\Exporter as JaegerExporter;
-use OpenTelemetry\SDK\AbstractClock;
 use OpenTelemetry\SDK\Attributes;
 use OpenTelemetry\SDK\Trace\Sampler\AlwaysOnSampler;
-use OpenTelemetry\SDK\Trace\SamplingResult;
-use OpenTelemetry\SDK\Trace\SpanProcessor\BatchSpanProcessor;
+use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
 use OpenTelemetry\SDK\Trace\TracerProvider;
 
-$sampler = new AlwaysOnSampler();
-$samplingResult = $sampler->shouldSample(
-    Context::getCurrent(),
-    md5((string) microtime(true)),
-    'io.opentelemetry.example',
-    API\SpanKind::KIND_INTERNAL
+$exporter = JaegerExporter::fromConnectionString('http://jaeger:9412/api/v2/spans', 'AlwaysOnJaegerExample');
+$tracerProvider = new TracerProvider(
+    new SimpleSpanProcessor($exporter),
+    new AlwaysOnSampler(),
 );
 
-$exporter = new JaegerExporter(
-    'alwaysOnJaegerExample',
-    'http://jaeger:9412/api/v2/spans',
-    new Client(),
-    new HttpFactory(),
-    new HttpFactory()
-);
+echo 'Starting AlwaysOnJaegerExample';
 
-if (SamplingResult::RECORD_AND_SAMPLE === $samplingResult->getDecision()) {
-    echo 'Starting AlwaysOnJaegerExample';
-    $tracer = (new TracerProvider())
-        ->addSpanProcessor(new BatchSpanProcessor($exporter, AbstractClock::getDefault()))
-        ->getTracer('io.opentelemetry.contrib.php');
+$tracer = $tracerProvider->getTracer();
 
-    for ($i = 0; $i < 5; $i++) {
-        // start a span, register some events
-        $timestamp = AbstractClock::getDefault()->timestamp();
-        $span = $tracer->startAndActivateSpan('session.generate.span' . microtime(true));
+$rootSpan = $tracer->spanBuilder('root')->startSpan();
+$rootSpan->activate();
 
-        $spanParent = $span->getParentContext();
-        echo sprintf(
-            PHP_EOL . 'Exporting Trace: %s, Parent: %s, Span: %s',
-            $span->getContext()->getTraceId(),
-            $spanParent ? $spanParent->getSpanId() : 'None',
-            $span->getContext()->getSpanId()
-        );
+for ($i = 0; $i < 5; $i++) {
+    // start a span, register some events
+    $span = $tracer->spanBuilder('session.generate.span-' . $i)->startSpan();
 
-        $span->setAttribute('remote_ip', '1.2.3.4')
-            ->setAttribute('country', 'USA');
+    echo sprintf(
+        PHP_EOL . 'Exporting Trace: %s, Parent: %s, Span: %s',
+        $span->getContext()->getTraceId(),
+        $span->getParentContext()->getSpanId() ?: 'None',
+        $span->getContext()->getSpanId()
+    );
 
-        $span->addEvent('found_login' . $i, new Attributes([
-            'id' => $i,
-            'username' => 'otuser' . $i,
-        ]), $timestamp);
-        $span->addEvent('generated_session', new Attributes([
-            'id' => md5((string) microtime(true)),
-        ]), $timestamp);
+    $span->setAttribute('remote_ip', '1.2.3.4')
+        ->setAttribute('country', 'USA');
 
-        try {
-            throw new Exception('Record exception test event');
-        } catch (Exception $exception) {
-            $span->recordException($exception);
-        }
+    $span->addEvent('found_login' . $i, new Attributes([
+        'id' => $i,
+        'username' => 'otuser' . $i,
+    ]));
+    $span->addEvent('generated_session', new Attributes([
+        'id' => md5((string) microtime(true)),
+    ]));
 
-        $span->end();
+    try {
+        throw new Exception('Record exception test event');
+    } catch (Exception $exception) {
+        $span->recordException($exception);
     }
-    echo PHP_EOL . 'AlwaysOnJaegerExample complete!  See the results at http://localhost:16686/';
-} else {
-    echo PHP_EOL . 'AlwaysOnJaegerExample tracing is not enabled';
+
+    $span->end();
 }
+$rootSpan->end();
+echo PHP_EOL . 'AlwaysOnJaegerExample complete!  See the results at http://localhost:16686/';
 
 echo PHP_EOL;
