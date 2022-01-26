ad86fd1941096a34459a479d89315c77a3825666
Grunet <sadikshahidain@gmail.com>
2022-01-26 03:37:50 +0000

    Moving the array serialization method to be more colocated

--- src/Contrib/Jaeger/SpanConverter.php
+++ src/Contrib/Jaeger/SpanConverter.php
@@ -181,35 +181,6 @@ class SpanConverter
         return $tags;
     }
 
-    private static function serializeArrayToString(array $arrayToSerialize): string
-    {
-        return self::recursivelySerializeArray($arrayToSerialize);
-    }
-
-    private static function recursivelySerializeArray($value): string
-    {
-        // Zipkin tags must be strings, but opentelemetry
-        // accepts strings, booleans, numbers, and lists of each.
-        if (is_array($value)) {
-            return join(',', array_map(function ($val) {
-                return self::recursivelySerializeArray($val);
-            }, $value));
-        }
-
-        // Casting false to string makes an empty string
-        if (is_bool($value)) {
-            return $value ? 'true' : 'false';
-        }
-
-        // Floats will lose precision if their string representation
-        // is >=14 or >=17 digits, depending on PHP settings.
-        // Can also throw E_RECOVERABLE_ERROR if $value is an object
-        // without a __toString() method.
-        // This is possible because OpenTelemetry\Trace\Span does not verify
-        // setAttribute() $value input.
-        return (string) $value;
-    }
-
     private static function buildTags(array $tagPairs): array
     {
         $tags = [];
@@ -257,6 +228,35 @@ class SpanConverter
             'vType' => TagType::STRING,
             'vStr' => (string)$value,
         ]);
+    }
+
+    private static function serializeArrayToString(array $arrayToSerialize): string
+    {
+        return self::recursivelySerializeArray($arrayToSerialize);
+    }
+
+    private static function recursivelySerializeArray($value): string
+    {
+        // Zipkin tags must be strings, but opentelemetry
+        // accepts strings, booleans, numbers, and lists of each.
+        if (is_array($value)) {
+            return join(',', array_map(function ($val) {
+                return self::recursivelySerializeArray($val);
+            }, $value));
+        }
+
+        // Casting false to string makes an empty string
+        if (is_bool($value)) {
+            return $value ? 'true' : 'false';
+        }
+
+        // Floats will lose precision if their string representation
+        // is >=14 or >=17 digits, depending on PHP settings.
+        // Can also throw E_RECOVERABLE_ERROR if $value is an object
+        // without a __toString() method.
+        // This is possible because OpenTelemetry\Trace\Span does not verify
+        // setAttribute() $value input.
+        return (string) $value;
     }
 
     private static function convertOtelEventsToJaegerLogs(SpanDataInterface $span): array
