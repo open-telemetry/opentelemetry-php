b80b96f335a6d7cab2d2bf1ed01ff098f809f2f7
Timo Michna <timomichna@yahoo.de>
2022-01-31 16:10:41 +0100

    Add env var constants

---
+++ src/SDK/Common/Environment/KnownValues.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\SDK\Common\Environment;
+
+use Psr\Log\LogLevel;
+
+/**
+ * "Known values" for OpenTelemetry environment variables.
+ * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md
+ * Notice: Values specific to the PHP SDK have been added
+ */
+interface KnownValues
+{
+    /**
+     * General SDK Configuration
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#general-sdk-configuration
+     */
+    public const OTEL_LOG_LEVEL = [
+        LogLevel::EMERGENCY,
+        LogLevel::ALERT,
+        LogLevel::CRITICAL,
+        LogLevel::ERROR,
+        LogLevel::WARNING,
+        LogLevel::NOTICE,
+        LogLevel::INFO,
+        LogLevel::DEBUG,
+    ];
+    public const OTEL_PROPAGATORS = [
+        'tracecontext', // W3C Trace Context
+        'baggage', // W3C Baggage
+        'b3', // B3 Single
+        'b3multi', // B3 Multi
+        'xray', // AWS X-Ray (third party)
+        'ottrace', // OT Trace (third party)
+        'none', // No automatically configured propagator.
+    ];
+    public const OTEL_TRACES_SAMPLER = [
+        'always_on',
+        'always_off',
+        'traceidratio',
+        'parentbased_always_on',
+        'parentbased_always_off',
+        'parentbased_traceidratio',
+        'jaeger_remote',
+        'xray',
+    ];
+    /**
+     * OTLP Exporter
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/protocol/exporter.md#configuration-options
+     */
+    // Insecure
+    public const OTEL_EXPORTER_OTLP_INSECURE = [
+        'true',
+        'false',
+        1,
+        0,
+    ];
+    public const OTEL_EXPORTER_OTLP_SPAN_INSECURE = [
+        'true',
+        'false',
+        1,
+        0,
+    ];
+    public const OTEL_EXPORTER_OTLP_METRIC_INSECURE = [
+        'true',
+        'false',
+        1,
+        0,
+    ];
+    // Compression
+    public const OTEL_EXPORTER_OTLP_COMPRESSION = ['gzip'];
+    public const OTEL_EXPORTER_OTLP_TRACES_COMPRESSION = ['gzip'];
+    public const OTEL_EXPORTER_OTLP_METRICS_COMPRESSION = ['gzip'];
+    // Protocol
+    public const OTEL_EXPORTER_OTLP_PROTOCOL = [
+        'grpc',
+        'http/protobuf',
+        'http/json',
+    ];
+    public const OTEL_EXPORTER_OTLP_TRACES_PROTOCOL = [
+        'grpc',
+        'http/protobuf',
+        'http/json',
+    ];
+    public const OTEL_EXPORTER_OTLP_METRICS_PROTOCOL = [
+        'grpc',
+        'http/protobuf',
+        'http/json',
+    ];
+    /**
+     * Exporter Selection
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#exporter-selection
+     */
+    public const OTEL_TRACES_EXPORTER = [
+        'otlp',
+        'jaeger',
+        'zipkin',
+        'newrelic',
+        'none',
+    ];
+    public const OTEL_METRICS_EXPORTER = [
+        'otlp',
+        'prometheus',
+        'none',
+    ];
+    public const OTEL_LOGS_EXPORTER = [
+        'otlp',
+        'none',
+    ];
+    /**
+     * Metrics SDK Configuration
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#metrics-sdk-configuration
+     */
+    public const OTEL_METRICS_EXEMPLAR_FILTER = [
+        'with_sampled_trace',
+        'all',
+        'none',
+    ];
+    /**
+     * Language Specific Environment Variables
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#language-specific-environment-variables
+     */
+    public const OTEL_PHP_TRACES_PROCESSOR = [
+        'batch',
+        'simple',
+        'noop',
+        'none',
+    ];
+}
---
+++ src/SDK/Common/Environment/ValueTypes.php
@@ -0,0 +1,121 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\SDK\Common\Environment;
+
+/**
+ * Environment variables defined by the OpenTelemetry specification and language specific variables for the PHP SDK.
+ * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md
+ */
+interface ValueTypes
+{
+    /**
+     * General SDK Configuration
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#general-sdk-configuration
+     */
+    public const OTEL_RESOURCE_ATTRIBUTES = VariableTypes::KEY_VALUE;
+    public const OTEL_SERVICE_NAME = VariableTypes::STRING;
+    public const OTEL_LOG_LEVEL = VariableTypes::ENUM;
+    public const OTEL_PROPAGATORS = VariableTypes::LIST;
+    public const OTEL_TRACES_SAMPLER = VariableTypes::STRING;
+    public const OTEL_TRACES_SAMPLER_ARG = VariableTypes::MIXED;
+    /**
+     * Batch Span Processor
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#batch-span-processor
+     */
+    public const OTEL_BSP_SCHEDULE_DELAY = VariableTypes::INTEGER;
+    public const OTEL_BSP_EXPORT_TIMEOUT = VariableTypes::INTEGER;
+    public const OTEL_BSP_MAX_QUEUE_SIZE = VariableTypes::INTEGER;
+    public const OTEL_BSP_MAX_EXPORT_BATCH_SIZE = VariableTypes::INTEGER;
+    /**
+     * Attribute Limits
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#attribute-limits
+     */
+    public const OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT = VariableTypes::INTEGER;
+    public const OTEL_ATTRIBUTE_COUNT_LIMIT = VariableTypes::INTEGER;
+    /**
+     * Span Limits
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#span-limits-
+     */
+    public const OTEL_SPAN_ATTRIBUTE_VALUE_LENGTH_LIMIT = VariableTypes::INTEGER;
+    public const OTEL_SPAN_ATTRIBUTE_COUNT_LIMIT = VariableTypes::INTEGER;
+    public const OTEL_SPAN_EVENT_COUNT_LIMIT = VariableTypes::INTEGER;
+    public const OTEL_SPAN_LINK_COUNT_LIMIT = VariableTypes::INTEGER;
+    public const OTEL_EVENT_ATTRIBUTE_COUNT_LIMIT = VariableTypes::INTEGER;
+    public const OTEL_LINK_ATTRIBUTE_COUNT_LIMIT = VariableTypes::INTEGER;
+    /**
+     * OTLP Exporter
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/protocol/exporter.md#configuration-options
+     */
+    // Endpoint
+    public const OTEL_EXPORTER_OTLP_ENDPOINT = VariableTypes::STRING;
+    public const OTEL_EXPORTER_OTLP_TRACES_ENDPOINT = VariableTypes::STRING;
+    public const OTEL_EXPORTER_OTLP_METRICS_ENDPOINT = VariableTypes::STRING;
+    // Insecure
+    public const OTEL_EXPORTER_OTLP_INSECURE = VariableTypes::BOOL;
+    public const OTEL_EXPORTER_OTLP_SPAN_INSECURE = VariableTypes::BOOL;
+    public const OTEL_EXPORTER_OTLP_METRIC_INSECURE = VariableTypes::BOOL;
+    // Certificate File
+    public const OTEL_EXPORTER_OTLP_CERTIFICATE = VariableTypes::STRING;
+    public const OTEL_EXPORTER_OTLP_TRACES_CERTIFICATE = VariableTypes::STRING;
+    public const OTEL_EXPORTER_OTLP_METRICS_CERTIFICATE = VariableTypes::STRING;
+    // Headers
+    public const OTEL_EXPORTER_OTLP_HEADERS = VariableTypes::KEY_VALUE;
+    public const OTEL_EXPORTER_OTLP_TRACES_HEADERS = VariableTypes::KEY_VALUE;
+    public const OTEL_EXPORTER_OTLP_METRICS_HEADERS = VariableTypes::KEY_VALUE;
+    // Compression
+    public const OTEL_EXPORTER_OTLP_COMPRESSION = VariableTypes::ENUM;
+    public const OTEL_EXPORTER_OTLP_TRACES_COMPRESSION = VariableTypes::ENUM;
+    public const OTEL_EXPORTER_OTLP_METRICS_COMPRESSION = VariableTypes::ENUM;
+    // Timeout
+    public const OTEL_EXPORTER_OTLP_TIMEOUT = VariableTypes::INTEGER;
+    public const OTEL_EXPORTER_OTLP_TRACES_TIMEOUT = VariableTypes::INTEGER;
+    public const OTEL_EXPORTER_OTLP_METRICS_TIMEOUT = VariableTypes::INTEGER;
+    // Protocol
+    public const OTEL_EXPORTER_OTLP_PROTOCOL = VariableTypes::ENUM;
+    public const OTEL_EXPORTER_OTLP_TRACES_PROTOCOL = VariableTypes::ENUM;
+    public const OTEL_EXPORTER_OTLP_METRICS_PROTOCOL = VariableTypes::ENUM;
+    /**
+     * Jaeger Exporter
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#jaeger-exporter
+     */
+    public const OTEL_EXPORTER_JAEGER_AGENT_HOST = VariableTypes::STRING;
+    public const OTEL_EXPORTER_JAEGER_AGENT_PORT = VariableTypes::INTEGER;
+    public const OTEL_EXPORTER_JAEGER_ENDPOINT = VariableTypes::STRING;
+    public const OTEL_EXPORTER_JAEGER_TIMEOUT = VariableTypes::INTEGER;
+    public const OTEL_EXPORTER_JAEGER_USER = VariableTypes::STRING;
+    public const OTEL_EXPORTER_JAEGER_PASSWORD = VariableTypes::STRING;
+    /**
+     * Zipkin Exporter
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#zipkin-exporter
+     */
+    public const OTEL_EXPORTER_ZIPKIN_ENDPOINT = VariableTypes::STRING;
+    public const OTEL_EXPORTER_ZIPKIN_TIMEOUT = VariableTypes::INTEGER;
+    public const OTEL_EXPORTER_ZIPKIN_PROTOCOL = VariableTypes::STRING;
+    /**
+     * Prometheus Exporter
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#prometheus-exporter
+     */
+    public const OTEL_EXPORTER_PROMETHEUS_HOST = VariableTypes::STRING;
+    public const OTEL_EXPORTER_PROMETHEUS_PORT = VariableTypes::INTEGER;
+    /**
+     * Exporter Selection
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#exporter-selection
+     */
+    public const OTEL_TRACES_EXPORTER = VariableTypes::STRING;
+    public const OTEL_METRICS_EXPORTER = VariableTypes::STRING;
+    public const OTEL_LOGS_EXPORTER = VariableTypes::STRING;
+    /**
+     * Metrics SDK Configuration
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#metrics-sdk-configuration
+     */
+    public const OTEL_METRICS_EXEMPLAR_FILTER = VariableTypes::ENUM;
+    public const OTEL_METRIC_EXPORT_INTERVAL = VariableTypes::INTEGER;
+    public const OTEL_METRIC_EXPORT_TIMEOUT = VariableTypes::INTEGER;
+    /**
+     * Language Specific Environment Variables
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#language-specific-environment-variables
+     */
+    public const OTEL_PHP_TRACES_PROCESSOR = VariableTypes::ENUM;
+}
