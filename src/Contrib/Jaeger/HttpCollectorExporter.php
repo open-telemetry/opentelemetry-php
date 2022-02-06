01ea880927fd427d36172dd1983636d4fdb12630
Grunet <sadikshahidain@gmail.com>
2022-02-06 22:24:47 +0000

    Scaffold the new exporter and helper class

---
+++ src/Contrib/Jaeger/HttpCollectorExporter.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\Contrib\Jaeger;
+
+use InvalidArgumentException;
+use OpenTelemetry\Contrib\Jaeger\ThriftHttpSender;
+use OpenTelemetry\SDK\Trace\Behavior\SpanExporterTrait;
+use OpenTelemetry\SDK\Trace\Behavior\UsesSpanConverterTrait;
+use OpenTelemetry\SDK\Trace\SpanExporterInterface;
+
+class HttpCollectorExporter implements SpanExporterInterface
+{
+    use UsesSpanConverterTrait;
+    use SpanExporterTrait;
+
+    private SpanConverter $spanConverter;
+
+    private ThriftHttpSender $sender;
+
+    public function __construct(
+        $name,
+        string $endpointUrl
+    ) {
+        $parsedDsn = parse_url($endpointUrl);
+
+        if (!is_array($parsedDsn)) {
+            throw new InvalidArgumentException('Unable to parse provided DSN');
+        }
+
+        if (!isset($parsedDsn['host']) || !isset($parsedDsn['port'])) {
+            throw new InvalidArgumentException('Endpoint should have host, port');
+        }
+        
+        $this->sender = new ThriftHttpSender(
+            $name,
+            $parsedDsn['host'], 
+            $parsedDsn['port'],
+        );
+
+        $this->spanConverter = new SpanConverter();
+    }
+
+    /**
+     * @psalm-return SpanExporterInterface::STATUS_*
+     */
+    public function doExport(iterable $spans): int
+    {
+        $thriftSpans = [];
+        foreach ($spans as $span) {
+            $thriftSpans[] = $this->spanConverter->convert($span);
+        }
+
+        $this->sender->send($thriftSpans);
+
+        return SpanExporterInterface::STATUS_SUCCESS;
+    }
+
+    /** @inheritDoc */
+    public static function fromConnectionString(string $endpointUrl, string $name, $args = null): HttpCollectorExporter
+    {
+        return new HttpCollectorExporter(
+            $name,
+            $endpointUrl
+        );
+    }
+}
---
+++ src/Contrib/Jaeger/ThriftHttpSender.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\Contrib\Jaeger;
+
+use Jaeger\Thrift\Batch;
+use Jaeger\Thrift\Process;
+use Jaeger\Thrift\Span as JTSpan;
+use Jaeger\Thrift\Tag;
+use OpenTelemetry\SDK\Behavior\LogsMessagesTrait;
+use Thrift\Protocol\TBinaryProtocol;
+use Thrift\Protocol\TProtocol;
+use Thrift\Transport\THttpClient;
+
+class ThriftHttpSender
+{
+    use LogsMessagesTrait;
+
+    private string $serviceName;
+
+    private TProtocol $protocol;
+
+    public function __construct(
+        string $serviceName,
+        string $host,
+        string $port
+    ) {
+        $this->serviceName = $serviceName;
+        
+        $transport = new THttpClient(
+            $host,
+            $port,
+        );
+        $this->protocol = new TBinaryProtocol($transport);
+    }
+
+    /**
+     * @param JTSpan[] $spans
+     */
+    public function send(array $spans): void
+    {
+        if (empty($spans)) {
+            return;
+        }
+
+        /** @var Tag[] $tags */
+        $tags = [];
+
+        //TODO - determine what of this is still needed and how to adapt it for spec compliance
+        // foreach ($this->tracer->getTags() as $k => $v) {
+        //     if (!in_array($k, $this->mapper->getSpecialSpanTags())) {
+        //         if (strpos($k, $this->mapper->getProcessTagsPrefix()) !== 0) {
+        //             continue ;
+        //         }
+
+        //         $quoted = preg_quote($this->mapper->getProcessTagsPrefix());
+        //         $k = preg_replace(sprintf('/^%s/', $quoted), '', $k);
+        //     }
+
+        //     if ($k === JAEGER_HOSTNAME_TAG_KEY) {
+        //         $k = "hostname";
+        //     }
+
+        //     $tags[] = new Tag([
+        //         "key" => $k,
+        //         "vType" => TagType::STRING,
+        //         "vStr" => $v
+        //     ]);
+        // }
+
+        // $tags[] = new Tag([
+        //     "key" => "format",
+        //     "vType" => TagType::STRING,
+        //     "vStr" => "jaeger.thrift"
+        // ]);
+
+        // $tags[] = new Tag([
+        //     "key" => "ip",
+        //     "vType" => TagType::STRING,
+        //     "vStr" => $this->tracer->getIpAddress()
+        // ]);
+
+        $batch = new Batch([
+            "spans" => $spans,
+            "process" => new Process([
+                "serviceName" => $this->serviceName,
+                "tags" => $tags
+            ])
+        ]);
+
+        $batch->write($this->protocol);
+        $this->protocol->getTransport()->flush();
+    }
+}
