d828a67db157867ebe7eae7160517353aab194f0
Grunet <sadikshahidain@gmail.com>
2022-02-27 01:13:28 +0000

    Revert ternary condensation commit from earlier

--- src/Contrib/Jaeger/HttpCollectorExporter.php
+++ src/Contrib/Jaeger/HttpCollectorExporter.php
@@ -36,8 +36,8 @@ class HttpCollectorExporter implements SpanExporterInterface
             $name,
             $parsedDsn['host'],
             $parsedDsn['port'],
-            $parsedDsn['path'] ?: '', //Matching THttpClient's default
-            $parsedDsn['scheme'] ?: 'http' //Matching THttpClient's default
+            isset($parsedDsn['path']) ? $parsedDsn['path'] : '', //Matching THttpClient's default
+            isset($parsedDsn['scheme']) ? $parsedDsn['scheme'] : 'http' //Matching THttpClient's default
         );
 
         $this->spanConverter = new SpanConverter();
