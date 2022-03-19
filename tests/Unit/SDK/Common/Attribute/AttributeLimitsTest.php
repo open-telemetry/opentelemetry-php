15b3e7ee8e318ad2b7d260a0d308d44fff4b7fd7
Timo Michna <timomichna@yahoo.de>
2022-03-20 00:44:23 +0100

    Move compare test to correct test-class

--- tests/Unit/SDK/Common/Attribute/AttributeLimitsTest.php
+++ tests/Unit/SDK/Common/Attribute/AttributeLimitsTest.php
@@ -12,6 +12,16 @@ use PHPUnit\Framework\TestCase;
  */
 class AttributeLimitsTest extends TestCase
 {
+    public function test_compare(): void
+    {
+        $attrLimits1 = new AttributeLimits(10, 20);
+        $attrLimits2 = new AttributeLimits(10, 20);
+        $attrLimits3 = new AttributeLimits(20, 30);
+
+        $this->assertEquals($attrLimits1, $attrLimits2);
+        $this->assertNotEquals($attrLimits1, $attrLimits3);
+    }
+
     public function test_default_limits(): void
     {
         $limits = new AttributeLimits();
