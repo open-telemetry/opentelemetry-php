26b8f9eb44ccb29fa73f4973a2f40adb839d006d
Timo Michna <timomichna@yahoo.de>
2022-03-25 13:09:18 +0100

    Finalize ClockFactory

--- src/SDK/Common/Time/ClockFactory.php
+++ src/SDK/Common/Time/ClockFactory.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\SDK\Common\Time;
 
-class ClockFactory
+final class ClockFactory implements ClockFactoryInterface
 {
     private static ?ClockInterface $default;
 
