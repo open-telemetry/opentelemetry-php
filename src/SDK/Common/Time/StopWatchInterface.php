08d82ac1e80d80e7f7b1afc3afbefd7c367e6aa7
Timo Michna <timomichna@yahoo.de>
2022-03-25 13:04:48 +0100

    Fix StopwatchInterface

--- src/SDK/Common/Time/StopWatchInterface.php
+++ src/SDK/Common/Time/StopWatchInterface.php
@@ -6,5 +6,15 @@ namespace OpenTelemetry\SDK\Common\Time;
 
 interface StopWatchInterface
 {
+    public function isRunning(): bool;
 
+    public function start(): void;
+
+    public function stop(): void;
+
+    public function reset(): void;
+
+    public function getElapsedTime(): int;
+
+    public function getLastElapsedTime(): int;
 }
