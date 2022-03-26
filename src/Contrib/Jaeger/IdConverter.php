0538c0bff15218f7dc07d62981c39187fc0be3de
Grunet <sadikshahidain@gmail.com>
2022-03-26 20:08:48 +0000

    Further isolating the soon-to-be-replaced use of intval

--- src/Contrib/Jaeger/IdConverter.php
+++ src/Contrib/Jaeger/IdConverter.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\Contrib\Jaeger;
 
+use Brick\Math\BigInteger;
+
+/**
+ * An implementation of the section of the spec around converting OTEL's span/trace id representations to Thrift's format
+ * https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/sdk_exporters/jaeger.md#ids
+ */
 class IdConverter
 {
     public static function convertOtelToJaegerTraceIds(string $traceId): array
     {
-        $traceIdLow = intval(substr($traceId, 0, 16), 16);
-        $traceIdHigh = intval(substr($traceId, 16, 32), 16);
+        $traceIdLow = self::convertHexStringToSignedInt(substr($traceId, 0, 16));
+        $traceIdHigh = self::convertHexStringToSignedInt(substr($traceId, 16, 32));
 
         return [
             'traceIdLow' => $traceIdLow,
@@ -19,6 +25,11 @@ class IdConverter
 
     public static function convertOtelToJaegerSpanId(string $spanId): int
     {
-        return intval($spanId, 16);
+        return self::convertHexStringToSignedInt($spanId);
+    }
+
+    private static function convertHexStringToSignedInt(string $hexString): int
+    {
+        return intval($hexString, 16);
     }
 }
