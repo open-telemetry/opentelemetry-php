b9dc9f492855736646f5048a8a49909a63b1fd51
yuktea <73362292+yuktea@users.noreply.github.com>
2022-03-26 23:24:23 +0530

    Compliance tests for environment variables
    Integration tests to verify compliance with the specification matrix.
    Covers
    OTEL_SPAN_ATTRIBUTE_VALUE_LENGTH_LIMIT
    OTEL_SPAN_EVENT_COUNT_LIMIT

---
+++ tests/Integration/SDK/SpanLimitsBuilderTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\Tests\Integration\SDK\Trace;
+
+use AssertWell\PHPUnitGlobalState\EnvironmentVariables;
+use OpenTelemetry\SDK\Trace\SpanLimitsBuilder;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers OpenTelemetry\SDK\Trace\SpanLimitsBuilder
+ */
+class SpanLimitsBuilderTest extends TestCase
+{
+    use EnvironmentVariables;
+
+    /**
+     * @group trace-compliance
+     */
+    public function test_span_length_limits_builder_uses_environment_variable(): void
+    {
+        $this->setEnvironmentVariable('OTEL_SPAN_ATTRIBUTE_VALUE_LENGTH_LIMIT', 9);
+        $builder = new SpanLimitsBuilder();
+        $spanLimits = $builder->build();
+        $this->assertEquals(9, $spanLimits->getAttributeLimits()->getAttributeValueLengthLimit());
+    }
+
+    /**
+     * @group trace-compliance
+     */
+    public function test_span_length_limits_builder_uses_configured_value(): void
+    {
+        $this->setEnvironmentVariable('OTEL_SPAN_ATTRIBUTE_VALUE_LENGTH_LIMIT', 9);
+        $builder = new SpanLimitsBuilder();
+        $builder->setAttributeValueLengthLimit(201);
+        $spanLimits = $builder->build();
+        $this->assertEquals(201, $spanLimits->getAttributeLimits()->getAttributeValueLengthLimit());
+    }
+
+    /**
+     * @group trace-compliance
+     */
+    public function test_span_event_limits_builder_uses_environment_variable(): void
+    {
+        $this->setEnvironmentVariable('OTEL_SPAN_EVENT_COUNT_LIMIT', 200);
+        $builder = new SpanLimitsBuilder();
+        $spanLimits = $builder->build();
+        $this->assertEquals(200, $spanLimits->getEventCountLimit());
+    }
+
+    /**
+     * @group trace-compliance
+     */
+    public function test_span_event_limits_builder_uses_configured_value(): void
+    {
+        $this->setEnvironmentVariable('OTEL_SPAN_EVENT_COUNT_LIMIT', 200);
+        $builder = new SpanLimitsBuilder();
+        $builder->setAttributeValueLengthLimit(185);
+        $spanLimits = $builder->build();
+        $this->assertEquals(185, $spanLimits->getEventCountLimit());
+    }
+}
