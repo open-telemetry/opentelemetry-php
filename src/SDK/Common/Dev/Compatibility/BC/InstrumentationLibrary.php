e7860dcb4479c58f9a33ed168d4249986b9b7de6
Timo Michna <timomichna@yahoo.de>
2022-03-27 19:26:28 +0200

    Add InstrumentationLibrary BC layer

---
+++ src/SDK/Common/Dev/Compatibility/BC/InstrumentationLibrary.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\SDK\Common\Dev\Compatibility\BC;
+
+use OpenTelemetry\SDK\Common\Instrumentation\InstrumentationLibrary as Moved;
+use OpenTelemetry\SDK\Common\Dev\Compatibility\Util;
+
+/**
+ * @codeCoverageIgnoreStart
+ */
+const OpenTelemetry_SDK_InstrumentationLibrary = 'OpenTelemetry\SDK\InstrumentationLibrary';
+
+final class InstrumentationLibrary implements InstrumentationLibraryInterface
+{
+    private Moved $adapted;
+
+    public function __construct(string $name, ?string $version = null, ?string $schemaUrl = null)
+    {
+        $this->adapted = new Moved($name, $version, $schemaUrl);
+
+        Util::triggerClassDeprecationNotice(
+            OpenTelemetry_SDK_InstrumentationLibrary,
+            Moved::class
+        );
+    }
+
+    public function getName(): string
+    {
+        return $this->adapted->getName();
+    }
+
+    public function getVersion(): ?string
+    {
+        return $this->adapted->getVersion();
+    }
+
+    public function getSchemaUrl(): ?string
+    {
+        return $this->adapted->getSchemaUrl();
+    }
+}
+
+class_alias(InstrumentationLibrary::class, OpenTelemetry_SDK_InstrumentationLibrary);
+/**
+ * @codeCoverageIgnoreEnd
+ */
--- src/SDK/Common/Dev/Compatibility/_load.php
+++ src/SDK/Common/Dev/Compatibility/_load.php
@@ -6,3 +6,5 @@ require_once __DIR__ . '/BC/AttributeLimitsInterface.php';
 require_once __DIR__ . '/BC/AttributeLimits.php';
 require_once __DIR__ . '/BC/AttributesInterface.php';
 require_once __DIR__ . '/BC/Attributes.php';
+require_once __DIR__ . '/BC/InstrumentationLibraryInterface.php';
+require_once __DIR__ . '/BC/InstrumentationLibrary.php';
