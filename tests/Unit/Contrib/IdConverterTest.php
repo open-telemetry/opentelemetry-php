a8895e2be463d81e57ef8112757707e948268c51
Grunet <sadikshahidain@gmail.com>
2022-03-31 01:49:41 +0000

    Add concrete example from Go for the trace id splitting

--- tests/Unit/Contrib/IdConverterTest.php
+++ tests/Unit/Contrib/IdConverterTest.php
@@ -20,7 +20,7 @@ class IdConverterTest extends TestCase
         $this->assertEquals(-72057594037927936, IdConverter::convertOtelToJaegerSpanId($hex));
     }
 
-    public function test_correctly_converted_random_span_id()
+    public function test_correctly_converts_random_span_id()
     {
         // 16 char hex string
         $hex = bin2hex(random_bytes(8));
@@ -28,14 +28,24 @@ class IdConverterTest extends TestCase
         $this->assertEquals($hex, $this->dechexWithLeadingZeroes(IdConverter::convertOtelToJaegerSpanId($hex)));
     }
 
-    public function test_correctly_converted_random_trace_id()
+    public function test_correctly_converts_trace_id()
+    {
+        $hex = "0102030405060708090a0b0c0d0e0f10";
+
+        $traceId = IdConverter::convertOtelToJaegerTraceIds($hex);
+
+        $this->assertEquals(72623859790382856, $traceId['traceIdHigh']);
+        $this->assertEquals(651345242494996240, $traceId['traceIdLow']);
+    }
+
+    public function test_correctly_converts_random_trace_id()
     {
         // 32 char hex string
         $hex = bin2hex(random_bytes(16));
 
         $traceId = IdConverter::convertOtelToJaegerTraceIds($hex);
-        $convertedTraceId = $this->dechexWithLeadingZeroes((int) $traceId['traceIdHigh']);
-        $convertedTraceId .= $this->dechexWithLeadingZeroes((int) $traceId['traceIdLow']);
+        $convertedTraceId = $this->dechexWithLeadingZeroes($traceId['traceIdHigh']);
+        $convertedTraceId .= $this->dechexWithLeadingZeroes($traceId['traceIdLow']);
         $this->assertEquals($hex, $convertedTraceId);
     }
 
