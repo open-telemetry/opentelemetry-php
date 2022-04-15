0f3665e75834cdb21c30d37757bc88e47744db97
ditsuke <ditsuke@protonmail.com>
2022-04-15 17:38:57 +0530

    Add test case for non-null version and null schema
    Also renames the other 2 tests for descriptiveness.

--- tests/Unit/SDK/Common/Instrumentation/KeyGeneratorTest.php
+++ tests/Unit/SDK/Common/Instrumentation/KeyGeneratorTest.php
@@ -12,7 +12,7 @@ use PHPUnit\Framework\TestCase;
  */
 class KeyGeneratorTest extends TestCase
 {
-    public function test_non_empty(): void
+    public function test_non_null(): void
     {
         $name = 'foo';
         $version = 'bar';
@@ -23,12 +23,22 @@ class KeyGeneratorTest extends TestCase
         $this->assertSame(sprintf('%s@%s %s', $name, $version, $schemaUrl), $key);
     }
 
-    public function test_null_version_schema(): void
+    public function test_null_version_and_null_schema(): void
     {
         $name = 'foo';
 
         $key = KeyGenerator::generateInstanceKey($name, null, null);
 
         $this->assertSame(sprintf('%s@unknown ', $name), $key);
+    }
+
+    public function test_non_null_version_and_null_schema(): void
+    {
+        $name = 'foo';
+        $version = 'bar';
+
+        $key = KeyGenerator::generateInstanceKey($name, $version, null);
+
+        $this->assertSame(sprintf('%s@%s ', $name, $version), $key);
     }
 }
