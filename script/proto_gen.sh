e2f3c55a9d585a6edb154f08cc709dba50995dcd
Brett McBride <brett.a.mcbride@gmail.com>
2022-04-25 22:57:49 +1000

    tidy up script

--- script/proto_gen.sh
+++ script/proto_gen.sh
@@ -8,24 +8,22 @@ cd "${DESTINATION_DIR}" || exit
 rm -rf ./${REPO_DIR}
 git clone https://github.com/open-telemetry/${REPO_DIR}
 
-for dir in ${REPO_DIR}
-do
-  (
-    cd ${dir}
-    TAG=$(
-        TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+(
+  cd ${REPO_DIR}
+  git config --add advice.detachedHead false
+  TAG=$(
+      TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 
-        git checkout "${TAG}"
+      git checkout "${TAG}"
 
-        echo "${TAG}"
-    )
-
-    echo "Generating protobuf files for version ${TAG} ..."
-    make gen-php
-    rm -rf ${GPBMETA_DIR} ${OTEL_DIR}
-    echo "${TAG}" > "${DESTINATION_DIR}/VERSION"
+      echo "${TAG}"
   )
-done
+
+  echo "Generating protobuf files for version ${TAG} ..."
+  make gen-php
+  rm -rf ${GPBMETA_DIR} ${OTEL_DIR}
+  echo "${TAG}" > "${DESTINATION_DIR}/VERSION"
+)
 
 echo "Copying generated source..."
 cp -r ${REPO_DIR}/gen/php/${GPBMETA_DIR} .
