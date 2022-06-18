d9406d7a8155a4e3f1652d68704a2dad5bbe85ca
yuktea <73362292+yuktea@users.noreply.github.com>
2022-06-18 16:52:29 +0530

    chore: Improve symfony-cli setup

--- tests/TraceContext/W3CTestService/trace-context-test.sh
+++ tests/TraceContext/W3CTestService/trace-context-test.sh
@@ -2,18 +2,14 @@
 set -x
 
 function install_symfony() {
-    curl -sS https://get.symfony.com/cli/installer | bash
-    mv /root/.symfony/bin/symfony /usr/local/bin/symfony
+    curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.alpine.sh' | bash >/dev/null
+    apk add symfony-cli
 }
 
 cd "tests/TraceContext/W3CTestService" || { echo "error: could not cd into the W3CTestService directory."; exit 1; }
 
 # Install Symfony: we will use the Symfony server as the built-in PHP server doesn't play well with duplicate headers
 install_symfony
-if [[ $? -ne 6 ]] # retry if we fail to install (flake)
-then
-    install_symfony
-fi
 
 # Start the test service in the background
 symfony server:start -d --port=8001 --no-tls
