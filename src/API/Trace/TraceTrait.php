5460fb7d09d351df690c319cebc60648c1af64a7
Grunet <sadikshahidain@gmail.com>
2022-06-20 22:11:02 +0000

    Fix to using an SDK class instead of API one for the fromContext call

--- src/API/Trace/TraceTrait.php
+++ src/API/Trace/TraceTrait.php
@@ -5,13 +5,12 @@ declare(strict_types=1);
 namespace OpenTelemetry\API\Trace;
 
 use OpenTelemetry\Context\Context;
-use OpenTelemetry\SDK\Trace\Span;
 
 trait TraceTrait
 {
     protected static function getCurrentSpan(): SpanInterface
     {
-        return Span::fromContext(Context::getCurrent());
+        return AbstractSpan::fromContext(Context::getCurrent());
     }
 
     protected static function setSpanIntoNewContext(SpanInterface $span): Context
