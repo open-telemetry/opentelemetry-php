abbc50dbcb8a9760d127e2dc5713e89e076f6a4b
Timo Michna <timomichna@yahoo.de>
2022-06-23 19:51:47 +0200

    Rework InstrumentationTrait

--- src/API/Common/Instrumentation/InstrumentationTrait.php
+++ src/API/Common/Instrumentation/InstrumentationTrait.php
@@ -4,45 +4,117 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\API\Common\Instrumentation;
 
+use OpenTelemetry\API\Metrics\MeterInterface;
 use OpenTelemetry\API\Metrics\MeterProviderInterface;
-use OpenTelemetry\API\Metrics\NoopMeterProvider;
-use OpenTelemetry\API\Trace\NoopTracerProvider;
+use OpenTelemetry\API\Metrics\NoopMeter;
+use OpenTelemetry\API\Trace\NoopTracer;
+use OpenTelemetry\API\Trace\TracerInterface;
 use OpenTelemetry\API\Trace\TracerProviderInterface;
 use OpenTelemetry\Context\Propagation\NullPropagator;
 use OpenTelemetry\Context\Propagation\TextMapPropagatorInterface;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
+use RuntimeException;
 
+/**
+ This trait in conjunction with the InstrumentationInterface is meant as a base for instrumentations for the
+ OpenTelemetry API.
+ Instrumentations need to implement the abstract methods of this trait (besides any instrumentation specific code)
+
+ A very simplified instrumentation could look like this:
+
+class Instrumentation implements InstrumentationInterface
+{
+    use InstrumentationTrait;
+
+    public function getName(): string
+    {
+        return 'foo-instrumentation';
+    }
+
+    public function getVersion(): ?string
+    {
+        return '0.0.1';
+    }
+
+    public function getSchemaUrl(): ?string
+    {
+        return null;
+    }
+
+    public function init(): bool
+    {
+        // This is just an example. In a real-world scenario one should only create spans in reaction of things
+        // happening in the instrumented code, not just for the sake of it.
+        $span = $this->getTracer()->spanBuilder($this->getName())->startSpan();
+        // do stuff
+        $span->end();
+    }
+}
+
+An user of the instrumentation and API/SDK would the call:
+
+$instrumentation = Instrumentation::create();
+$instrumentation->activate()
+
+to activate and use the instrumentation with the API/SDK.
+
+ **/
 trait InstrumentationTrait
 {
     private TextMapPropagatorInterface $propagator;
-    private TracerProviderInterface $tracerProvider;
-    private MeterProviderInterface $meterProvider;
+    private TracerInterface $tracer;
+    private MeterInterface $meter;
     private LoggerInterface $logger;
 
     public function __construct()
     {
-        $this->propagator = new NullPropagator();
-        $this->tracerProvider = new NoopTracerProvider();
-        $this->meterProvider = new NoopMeterProvider();
-        $this->logger = new NullLogger();
+        $this->initDefaults();
     }
 
+    public static function create(): InstrumentationInterface
+    {
+        return new static();
+    }
+
+    /**
+     * The name of the instrumenting/instrumented library/package/project.
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/v1.12.0/specification/glossary.md#instrumentation-scope
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/v1.12.0/specification/glossary.md#instrumentation-library
+     */
     abstract public function getName(): string;
 
+    /**
+     * The version of the instrumenting/instrumented library/package/project.
+     * If unknown or a lookup is too expensive simply return NULL.
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/v1.12.0/specification/glossary.md#instrumentation-scope
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/v1.12.0/specification/glossary.md#instrumentation-library
+     */
     abstract public function getVersion(): ?string;
 
-    abstract public function getSchema(): ?string;
+    /**
+     * The version of the instrumenting/instrumented library/package/project.
+     * If unknown simply return NULL.
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/v1.12.0/specification/glossary.md#instrumentation-scope
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/v1.12.0/specification/glossary.md#instrumentation-library
+     */
+    abstract public function getSchemaUrl(): ?string;
 
-    public function register(): bool
-    {
-        // not implemented yet
-        return true;
-    }
+    /**
+     * This method will be called from the API when the instrumentation has been activated (via activate()).
+     * Here you can put any bootstrapping code needed by the instrumentation.
+     * If not needed simply implement a method which returns TRUE.
+     */
+    abstract public function init(): bool;
 
+    /**
+     * This method registers and activates the instrumentation with the OpenTelemetry API/SDK and thus
+     * the instrumentation will be used to generate telemetry data.
+     */
     public function activate(): bool
     {
-        // not implemented yet
+        $this->validateImplementation();
+        // activate instrumentation with the API. not implemented yet.
         return true;
     }
 
@@ -58,22 +130,31 @@ trait InstrumentationTrait
 
     public function setTracerProvider(TracerProviderInterface $tracerProvider): void
     {
-        $this->tracerProvider = $tracerProvider;
+        // @see https://github.com/open-telemetry/opentelemetry-specification/blob/v1.12.0/specification/trace/api.md#get-a-tracer
+        $this->tracer = $tracerProvider->getTracer(
+            $this->getName(),
+            $this->getVersion(),
+            $this->getSchemaUrl(),
+        );
     }
 
-    public function getTracerProvider(): TracerProviderInterface
+    public function getTracer(): TracerInterface
     {
-        return $this->tracerProvider;
+        return $this->tracer;
     }
 
     public function setMeterProvider(MeterProviderInterface $meterProvider): void
     {
-        $this->meterProvider = $meterProvider;
+        // @see https://github.com/open-telemetry/opentelemetry-specification/blob/v1.12.0/specification/metrics/api.md#get-a-meter
+        $this->meter = $meterProvider->getMeter(
+            $this->getName(),
+            $this->getVersion(),
+        );
     }
 
-    public function getMeterProvider(): MeterProviderInterface
+    public function getMeter(): MeterInterface
     {
-        return $this->meterProvider;
+        return $this->meter;
     }
 
     public function setLogger(LoggerInterface $logger): void
@@ -84,5 +165,24 @@ trait InstrumentationTrait
     public function getLogger(): LoggerInterface
     {
         return $this->logger;
+    }
+
+    private function validateImplementation(): void
+    {
+        if (!$this instanceof InstrumentationInterface) {
+            throw new RuntimeException(sprintf(
+                '"%s" is meant to implement "%s"',
+                InstrumentationTrait::class,
+                InstrumentationInterface::class
+            ));
+        }
+    }
+
+    private function initDefaults(): void
+    {
+        $this->propagator = new NullPropagator();
+        $this->tracer = new NoopTracer();
+        $this->meter = new NoopMeter();
+        $this->logger = new NullLogger();
     }
 }
