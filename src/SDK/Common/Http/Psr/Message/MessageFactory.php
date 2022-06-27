275a4a757672f0fb0439dee59e661f9bb65e672c
Timo Michna <timomichna@yahoo.de>
2022-06-27 12:29:26 +0200

    Add MessageFactory

--- src/SDK/Common/Http/Psr/Message/MessageFactory.php
+++ src/SDK/Common/Http/Psr/Message/MessageFactory.php
@@ -11,7 +11,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestFactoryInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-final class MessageFactory implements MessageFactoryInterface
+class MessageFactory implements MessageFactoryInterface
 {
     private RequestFactoryInterface $requestFactory;
     private ResponseFactoryInterface $responseFactory;
