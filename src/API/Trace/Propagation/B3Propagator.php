b15bd136162020ef3894a6521c7324edfd7da97a
Kishan Sangani <kisangan@cisco.com>
2022-07-27 11:52:21 +0530

    Changed B3Propagator class

--- src/API/Trace/Propagation/B3Propagator.php
+++ src/API/Trace/Propagation/B3Propagator.php
@@ -4,69 +4,21 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\API\Trace\Propagation;
 
-use OpenTelemetry\Context\Context;
-use OpenTelemetry\Context\Propagation\ArrayAccessGetterSetter;
-use OpenTelemetry\Context\Propagation\PropagationGetterInterface;
-use OpenTelemetry\Context\Propagation\PropagationSetterInterface;
 use OpenTelemetry\Context\Propagation\TextMapPropagatorInterface;
 
 /**
  * B3 is a propagator that supports the specification for the header
  * "b3" used for trace context propagation across service boundaries.
- * (https://github.com/openzipkin/b3-propagation#single-header)
+ * (https://github.com/openzipkin/b3-propagation)
  */
-final class B3Propagator implements TextMapPropagatorInterface
+final class B3Propagator
 {
-    private TextMapPropagatorInterface $propagator;
-
-    private static ?self $instance = null;
-
-    private function __construct()
+    public static function getB3SingleHeaderInstance(): TextMapPropagatorInterface
     {
-        $propagatorList = getenv('OTEL_PROPAGATORS');
-        /**
-         * @phpstan-ignore-next-line
-         */
-        if ((is_array($propagatorList) && in_array('b3Multi', $propagatorList)) || ('b3Multi' === $propagatorList)) {
-            $this->propagator = B3MultiPropagator::getInstance();
-        } else {
-            $this->propagator = B3SinglePropagator::getInstance();
-        }
+        return B3SinglePropagator::getInstance();
     }
-
-    public static function getInstance(): self
+    public static function getB3MultiHeaderInstance(): TextMapPropagatorInterface
     {
-        if (null === self::$instance) {
-            self::$instance = new self();
-        }
-
-        return self::$instance;
-    }
-
-    /** {@inheritdoc} */
-    public function fields(): array
-    {
-        return $this->propagator->fields();
-    }
-
-    /** {@inheritdoc} */
-    public function inject(&$carrier, PropagationSetterInterface $setter = null, Context $context = null): void
-    {
-        $this->propagator->inject($carrier, $setter, $context);
-    }
-
-    /** {@inheritdoc} */
-    public function extract($carrier, PropagationGetterInterface $getter = null, Context $context = null): Context
-    {
-        $getter ??= ArrayAccessGetterSetter::getInstance();
-        $context ??= Context::getCurrent();
-
-        $b3Context = $getter->get($carrier, B3SinglePropagator::B3);
-
-        if ($b3Context) {
-            return B3SinglePropagator::getInstance()->extract($carrier, $getter, $context);
-        }
-
-        return B3MultiPropagator::getInstance()->extract($carrier, $getter, $context);
+        return B3MultiPropagator::getInstance();
     }
 }
--- tests/Unit/API/Trace/Propagation/B3PropagatorTest.php
+++ tests/Unit/API/Trace/Propagation/B3PropagatorTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\Tests\API\Unit\Trace\Propagation;
 
-use AssertWell\PHPUnitGlobalState\EnvironmentVariables;
 use OpenTelemetry\API\Trace\Propagation\B3Propagator;
 use PHPUnit\Framework\TestCase;
 
@@ -13,42 +12,21 @@ use PHPUnit\Framework\TestCase;
  */
 class B3PropagatorTest extends TestCase
 {
-    use EnvironmentVariables;
-
-    private B3Propagator $b3Propagator;
-
-    public function tearDown(): void
-    {
-        $this->restoreEnvironmentVariables();
-        unset($this->b3Propagator);
-    }
-
     public function test_b3multi_fields(): void
     {
-        $this->setEnvironmentVariable('OTEL_PROPAGATORS', 'b3Multi');
-        $this->b3Propagator = B3Propagator::getInstance();
+        $propagator = B3Propagator::getB3MultiHeaderInstance();
         $this->assertSame(
             ['X-B3-TraceId', 'X-B3-SpanId', 'X-B3-ParentSpanId', 'X-B3-Sampled', 'X-B3-Flags'],
-            $this->b3Propagator->fields()
+            $propagator->fields()
         );
     }
 
-//    public function test_default_fields(): void
-//    {
-//        $this->b3Propagator = B3Propagator::getInstance();
-//        $this->assertSame(
-//            ['b3'],
-//            $this->b3Propagator->fields()
-//        );
-//    }
-//
-//    public function test_b3single_fields(): void
-//    {
-//        $this->setEnvironmentVariable('OTEL_PROPAGATORS', 'b3');
-//        $this->b3Propagator = B3Propagator::getInstance();
-//        $this->assertSame(
-//            ['b3'],
-//            $this->b3Propagator->fields()
-//        );
-//    }
+    public function test_b3single_fields(): void
+    {
+        $propagator = B3Propagator::getB3SingleHeaderInstance();
+        $this->assertSame(
+            ['b3'],
+            $propagator->fields()
+        );
+    }
 }
