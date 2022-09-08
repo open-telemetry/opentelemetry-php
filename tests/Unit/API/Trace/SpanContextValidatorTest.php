9a782f8b7dae4a605ab1cca9dec2a3dd485abcc0
ambkhand <ambkhand@cisco.com>
2022-09-08 16:47:24 +0530

    updated failing test

--- tests/Unit/API/Trace/SpanContextValidatorTest.php
+++ tests/Unit/API/Trace/SpanContextValidatorTest.php
@@ -12,6 +12,9 @@ use PHPUnit\Framework\TestCase;
  */
 class SpanContextValidatorTest extends TestCase
 {
+    private const TRACE_ID = 'ff000000000000000000000000000041';
+    private const SPAN_ID = 'ff00000000000041';
+
     public function test_invalid_trace_id(): void
     {
         $this->assertFalse(SpanContextValidator::isValidTraceID(SpanContextValidator::INVALID_TRACE));
@@ -19,7 +22,7 @@ class SpanContextValidatorTest extends TestCase
 
     public function test_valid_trace_id(): void
     {
-        $this->assertTrue(SpanContextValidator::isValidTraceID(SpanContextValidator::VALID_TRACE));
+        $this->assertTrue(SpanContextValidator::isValidTraceID(self::TRACE_ID));
     }
 
     public function test_invalid_span_id(): void
@@ -29,6 +32,6 @@ class SpanContextValidatorTest extends TestCase
 
     public function test_valid_span_id(): void
     {
-        $this->assertTrue(SpanContextValidator::isValidSpanID(SpanContextValidator::VALID_SPAN));
+        $this->assertTrue(SpanContextValidator::isValidSpanID(self::SPAN_ID));
     }
 }
