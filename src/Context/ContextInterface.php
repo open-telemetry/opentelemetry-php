215731f6390e74068314fc719a541d13921b1f5c
Tobias Bachert <git@b-privat.de>
2022-09-23 20:15:46 +0200

    Improve `open-telemetry/context` documentation (#830)

--- src/Context/ContextInterface.php
+++ src/Context/ContextInterface.php
@@ -4,39 +4,83 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\Context;
 
+/**
+ * Immutable execution scoped propagation mechanism.
+ *
+ * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/context/README.md#context
+ */
 interface ContextInterface
 {
     /**
-     * @param non-empty-string $key
+     * Creates a new context key.
      *
-     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/v1.6.1/specification/context/context.md#create-a-key
+     * @param non-empty-string $key name of the key
+     * @return ContextKeyInterface created key
+     *
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/context/README.md#create-a-key
      */
     public static function createKey(string $key): ContextKeyInterface;
 
+    /**
+     * Returns the current context.
+     *
+     * @return ContextInterface current context
+     *
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/context/README.md#get-current-context
+     */
     public static function getCurrent(): ContextInterface;
 
     /**
-     * Makes `$this` the currently active {@see ContextInterface}.
+     * Attaches this context as active context.
+     *
+     * The returned scope has to be {@link ScopeInterface::detach()}ed. In most
+     * cases this should be done using a `try-finally` statement:
+     * ```php
+     * $scope = $context->activate();
+     * try {
+     *     // ...
+     * } finally {
+     *     $scope->detach();
+     * }
+     * ```
+     *
+     * @return ScopeInterface scope to detach the context and restore the previous
+     *         context
+     *
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/context/README.md#attach-context
      */
     public function activate(): ScopeInterface;
 
     /**
-     * This adds a key/value pair to this Context.
+     * Returns a context with the given key set to the given value.
      *
-     * @psalm-template T
-     * @psalm-param ContextKeyInterface<T> $key
-     * @psalm-param T|null $value
+     * @template T
+     * @param ContextKeyInterface<T> $key key to set
+     * @param T|null $value value to set
+     * @return ContextInterface a context with the given key set to `$value`
+     *
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/context/README.md#set-value
      */
     public function with(ContextKeyInterface $key, $value): ContextInterface;
 
+    /**
+     * Returns a context with the given value set.
+     *
+     * @param ImplicitContextKeyedInterface $value value to set
+     * @return ContextInterface a context with the given `$value`
+     *
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/context/README.md#set-value
+     */
     public function withContextValue(ImplicitContextKeyedInterface $value): ContextInterface;
 
     /**
-     * Fetch a value from the Context given a key value.
+     * Returns the value assigned to the given key.
      *
-     * @psalm-template T
-     * @psalm-param ContextKeyInterface<T> $key
-     * @psalm-return T|null
+     * @template T
+     * @param ContextKeyInterface<T> $key key to get
+     * @return T|null value assigned to `$key`, or null if no such value exists
+     *
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/context/README.md#get-value
      */
     public function get(ContextKeyInterface $key);
 }
--- src/Context/ContextStorageInterface.php
+++ src/Context/ContextStorageInterface.php
@@ -6,9 +6,27 @@ namespace OpenTelemetry\Context;
 
 interface ContextStorageInterface
 {
+    /**
+     * Returns the current scope.
+     *
+     * @return ContextStorageScopeInterface|null current scope, or null if no
+     *         scope was attached in the current execution unit
+     */
     public function scope(): ?ContextStorageScopeInterface;
 
+    /**
+     * Returns the current context.
+     *
+     * @return ContextInterface current context
+     */
     public function current(): ContextInterface;
 
+    /**
+     * Attaches the context as active context.
+     *
+     * @param ContextInterface $context context to attach
+     * @return ContextStorageScopeInterface scope to detach the context and
+     *         restore the previous context
+     */
     public function attach(ContextInterface $context): ContextStorageScopeInterface;
 }
