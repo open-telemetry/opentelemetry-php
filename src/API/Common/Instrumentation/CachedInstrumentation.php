f6671ddd796570d737e7d6e5d362a58c2faec9e0
Tobias Bachert <git@b-privat.de>
2022-10-16 23:57:57 +0200

    Add alternative instrumentation abstraction (#822)
    * Add alternative instrumentation abstraction
    * Provide global access to providers
    Remove logs for now, will be replaced with `LoggerProvider` in the future.

---
+++ src/API/Common/Instrumentation/CachedInstrumentation.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\API\Common\Instrumentation;
+
+use ArrayAccess;
+use function assert;
+use function class_exists;
+use OpenTelemetry\API\Metrics\MeterInterface;
+use OpenTelemetry\API\Metrics\MeterProviderInterface;
+use OpenTelemetry\API\Trace\TracerInterface;
+use OpenTelemetry\API\Trace\TracerProviderInterface;
+use const PHP_VERSION_ID;
+
+/**
+ * Provides access to cached {@link TracerInterface} and {@link MeterInterface}
+ * instances.
+ *
+ * Autoinstrumentation should prefer using a {@link CachedInstrumentation}
+ * instance over repeatedly obtaining instrumentation instances from
+ * {@link Globals}.
+ */
+final class CachedInstrumentation
+{
+    private string $name;
+    private ?string $version;
+    private ?string $schemaUrl;
+    private iterable $attributes;
+    /** @var ArrayAccess<TracerProviderInterface, TracerInterface>|null */
+    private ?ArrayAccess $tracers;
+    /** @var ArrayAccess<MeterProviderInterface, MeterInterface>|null */
+    private ?ArrayAccess $meters;
+
+    public function __construct(string $name, ?string $version = null, ?string $schemaUrl = null, iterable $attributes = [])
+    {
+        $this->name = $name;
+        $this->version = $version;
+        $this->schemaUrl = $schemaUrl;
+        $this->attributes = $attributes;
+        $this->tracers = self::createWeakMap();
+        $this->meters = self::createWeakMap();
+    }
+
+    private static function createWeakMap(): ?ArrayAccess
+    {
+        if (PHP_VERSION_ID < 80000) {
+            return null;
+        }
+
+        /** @phan-suppress-next-line PhanUndeclaredClassReference */
+        assert(class_exists(\WeakMap::class, false));
+        /** @phan-suppress-next-line PhanUndeclaredClassMethod */
+        $map = new \WeakMap();
+        assert($map instanceof ArrayAccess);
+
+        return $map;
+    }
+
+    public function tracer(): TracerInterface
+    {
+        $tracerProvider = Globals::tracerProvider();
+
+        if ($this->tracers === null) {
+            return $tracerProvider->getTracer($this->name, $this->version, $this->schemaUrl, $this->attributes);
+        }
+
+        return $this->tracers[$tracerProvider] ??= $tracerProvider->getTracer($this->name, $this->version, $this->schemaUrl, $this->attributes);
+    }
+
+    public function meter(): MeterInterface
+    {
+        $meterProvider = Globals::meterProvider();
+
+        if ($this->meters === null) {
+            return $meterProvider->getMeter($this->name, $this->version, $this->schemaUrl, $this->attributes);
+        }
+
+        return $this->meters[$meterProvider] ??= $meterProvider->getMeter($this->name, $this->version, $this->schemaUrl, $this->attributes);
+    }
+}
---
+++ src/API/Common/Instrumentation/Configurator.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\API\Common\Instrumentation;
+
+use OpenTelemetry\API\Metrics\MeterProviderInterface;
+use OpenTelemetry\API\Trace\TracerProviderInterface;
+use OpenTelemetry\Context\Context;
+use OpenTelemetry\Context\ContextInterface;
+use OpenTelemetry\Context\ImplicitContextKeyedInterface;
+use OpenTelemetry\Context\Propagation\TextMapPropagatorInterface;
+use OpenTelemetry\Context\ScopeInterface;
+
+/**
+ * Configures the global (context scoped) instrumentation instances.
+ *
+ * @see Configurator::activate()
+ */
+final class Configurator implements ImplicitContextKeyedInterface
+{
+    private ?TracerProviderInterface $tracerProvider = null;
+    private ?MeterProviderInterface $meterProvider = null;
+    private ?TextMapPropagatorInterface $propagator = null;
+
+    private function __construct()
+    {
+    }
+
+    public static function create(): Configurator
+    {
+        return new self();
+    }
+
+    public function activate(): ScopeInterface
+    {
+        return $this->storeInContext()->activate();
+    }
+
+    public function storeInContext(?ContextInterface $context = null): ContextInterface
+    {
+        $context ??= Context::getCurrent();
+
+        if ($this->tracerProvider !== null) {
+            $context = $context->with(ContextKeys::tracerProvider(), $this->tracerProvider);
+        }
+        if ($this->meterProvider !== null) {
+            $context = $context->with(ContextKeys::meterProvider(), $this->meterProvider);
+        }
+        if ($this->propagator !== null) {
+            $context = $context->with(ContextKeys::propagator(), $this->propagator);
+        }
+
+        return $context;
+    }
+
+    public function withTracerProvider(?TracerProviderInterface $tracerProvider): Configurator
+    {
+        $self = clone $this;
+        $self->tracerProvider = $tracerProvider;
+
+        return $self;
+    }
+
+    public function withMeterProvider(?MeterProviderInterface $meterProvider): Configurator
+    {
+        $self = clone $this;
+        $self->meterProvider = $meterProvider;
+
+        return $self;
+    }
+
+    public function withPropagator(?TextMapPropagatorInterface $propagator): Configurator
+    {
+        $self = clone $this;
+        $self->propagator = $propagator;
+
+        return $self;
+    }
+}
