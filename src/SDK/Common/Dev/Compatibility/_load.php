c6b0be7c89a8482b2cf49be20f72e6256ddc4a30
Brett McBride <brett.a.mcbride@gmail.com>
2023-02-14 10:56:50 +1100

    remove older BC implementations (#923)
    These BC features have been in place for multiple releases, it's time for them to retire.

--- src/SDK/Common/Dev/Compatibility/_load.php
+++ src/SDK/Common/Dev/Compatibility/_load.php
@@ -2,10 +2,5 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/BC/ClockInterface.php';
-require_once __DIR__ . '/BC/AbstractClock.php';
-require_once __DIR__ . '/BC/SystemClock.php';
-require_once __DIR__ . '/BC/InstrumentationLibraryInterface.php';
-require_once __DIR__ . '/BC/InstrumentationLibrary.php';
 require_once __DIR__ . '/BC/OtlpExporter.php';
 require_once __DIR__ . '/BC/LoggerHolder.php';
