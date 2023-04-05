9cb01ba7a144c0497af8d603e428901ac69a3c31
Brett McBride <brett@deakin.edu.au>
2023-04-05 15:41:54 +1000

    clarify bridge api not for developers

--- src/API/Logs/Bridge.php
+++ src/API/Logs/Bridge.php
@@ -8,7 +8,9 @@ use OpenTelemetry\API\Common\Instrumentation\Globals;
 
 /**
  * OpenTelemetry logs bridge API. To be used when implementing a handler/appender for existing logging libraries.
- * This API is not meant to be used by application developers.
+ * This API is not meant to be used by application developers, but rather by library developers for the purpose
+ * of integrating existing logging libraries with OpenTelemetry.
+ *
  * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/logs/bridge-api.md
  */
 class Bridge
