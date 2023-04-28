774dfcd970ab135c08412305d9f8a295fca8b34a
Brett McBride <brett@deakin.edu.au>
2023-04-28 13:52:52 +1000

    resource info spec compliance, instrumentation attribute when merging resources, later (updating) keys should override existing keys, per spec. Update to work this way, and adjust some tests accordingly. Reorganise detectors so that environment runs last (per spec, OTEL_SERVICE_NAME should be the highest priority way of setting service name) Adding the experimental telemetry.auto.version attribute, if the auto-instrumentation extension is installed.

--- tests/Unit/SDK/Resource/ResourceInfoFactoryTest.php
+++ tests/Unit/SDK/Resource/ResourceInfoFactoryTest.php
@@ -29,6 +29,9 @@ class ResourceInfoFactoryTest extends TestCase
         $this->assertEmpty($resource->getAttributes());
     }
 
+    /**
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/v1.20.0/specification/resource/sdk.md#merge
+     */
     public function test_merge(): void
     {
         $primary = ResourceInfo::create(Attributes::create(['name' => 'primary', 'empty' => '']));
@@ -42,7 +45,21 @@ class ResourceInfoFactoryTest extends TestCase
         $this->assertCount(3, $result->getAttributes());
         $this->assertEquals('primary', $name);
         $this->assertEquals('1.0.0', $version);
-        $this->assertEquals('', $empty);
+        $this->assertEquals('value', $empty);
+    }
+
+    /**
+     * @group compliance
+     * "If a key exists on both the old and updating resource, the value of the updating resource MUST be picked (even if the updated value is empty)"
+     */
+    public function test_merge_uses_value_of_updating_resource(): void
+    {
+        $old = ResourceInfo::create(Attributes::create(['name' => 'original', 'foo' => 'bar']));
+        $updating = ResourceInfo::create(Attributes::create(['name' => 'updated', 'foo' => '']));
+        $merged = ResourceInfoFactory::merge($old, $updating);
+
+        $this->assertSame('updated', $merged->getAttributes()->get('name'));
+        $this->assertSame('', $merged->getAttributes()->get('foo'));
     }
 
     /**
