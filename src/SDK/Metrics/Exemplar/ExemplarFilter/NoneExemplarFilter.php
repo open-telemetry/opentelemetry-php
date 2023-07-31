ff15cc7037cbdb58641785434b61fd1aa9f27085
Brett McBride <brett.a.mcbride@gmail.com>
2023-08-01 08:09:07 +1000

    document that examplars are not stable (#1091)
    noted in TC review. Java moved their examplar code into an internal package, and
    documented that it may change. We don't have an internal package, so document
    the warning in a few places.

--- src/SDK/Metrics/Exemplar/ExemplarFilter/NoneExemplarFilter.php
+++ src/SDK/Metrics/Exemplar/ExemplarFilter/NoneExemplarFilter.php
@@ -8,6 +8,10 @@ use OpenTelemetry\Context\ContextInterface;
 use OpenTelemetry\SDK\Common\Attribute\AttributesInterface;
 use OpenTelemetry\SDK\Metrics\Exemplar\ExemplarFilterInterface;
 
+/**
+ * The exemplar spec is not yet stable, and can change at any time.
+ * see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/sdk.md#exemplar
+ */
 final class NoneExemplarFilter implements ExemplarFilterInterface
 {
     public function accepts($value, AttributesInterface $attributes, ContextInterface $context, int $timestamp): bool
--- src/SDK/Metrics/Exemplar/ExemplarFilterInterface.php
+++ src/SDK/Metrics/Exemplar/ExemplarFilterInterface.php
@@ -7,6 +7,10 @@ namespace OpenTelemetry\SDK\Metrics\Exemplar;
 use OpenTelemetry\Context\ContextInterface;
 use OpenTelemetry\SDK\Common\Attribute\AttributesInterface;
 
+/**
+ * The exemplar spec is not yet stable, and can change at any time.
+ * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/sdk.md#exemplar
+ */
 interface ExemplarFilterInterface
 {
     /**
