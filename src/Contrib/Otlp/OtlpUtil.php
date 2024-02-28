b592c589b52f6ee6632d9f6eb91ac72d878db778
Brett McBride <brett.a.mcbride@gmail.com>
2024-02-29 08:53:30 +1100

    RFC3986 decode
    Co-authored-by: Tobias Bachert <git@b-privat.de>

--- src/Contrib/Otlp/OtlpUtil.php
+++ src/Contrib/Otlp/OtlpUtil.php
@@ -44,7 +44,7 @@ class OtlpUtil
             Configuration::getMap(Variables::OTEL_EXPORTER_OTLP_HEADERS);
         $headers += self::getUserAgentHeader();
 
-        return array_map('urldecode', $headers);
+        return array_map('rawurldecode', $headers);
     }
 
     /**
