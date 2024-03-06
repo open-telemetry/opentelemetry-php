231ac8a28b12de4e994ca99a32ee8572ba48a1dc
Tobias Bachert <git@b-privat.de>
2024-03-06 17:57:56 +0100

    Add configuration example that utilizes env substitution

---
+++ examples/load_config_env.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+use OpenTelemetry\API\Logs\EventLogger;
+use OpenTelemetry\API\Logs\LogRecord;
+use OpenTelemetry\Config\SDK\Configuration;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+echo 'load config SDK example starting...' . PHP_EOL;
+
+$_SERVER['OTEL_SERVICE_NAME'] = 'opentelemetry-demo';
+$_SERVER['OTEL_EXPORTER_OTLP_PROTOCOL'] = 'http/protobuf';
+$_SERVER['OTEL_EXPORTER_OTLP_ENDPOINT'] = 'http://collector:4318';
+$_SERVER['OTEL_TRACES_SAMPLER_ARG'] = '0.5';
+
+$config = Configuration::parseFile(__DIR__ . '/load_config_env.yaml');
+$sdk = $config
+    ->create()
+    ->setAutoShutdown(true)
+    ->build();
+
+$tracer = $sdk->getTracerProvider()->getTracer('demo');
+$meter = $sdk->getMeterProvider()->getMeter('demo');
+$logger = $sdk->getLoggerProvider()->getLogger('demo');
+
+$tracer->spanBuilder('root')->startSpan()->end();
+$meter->createCounter('cnt')->add(1);
+
+$eventLogger = new EventLogger($logger, 'my-domain');
+$eventLogger->logEvent('foo', new LogRecord('hello, otel'));
+
+echo 'Finished!' . PHP_EOL;
