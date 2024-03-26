68e6b091fd0d21ac049c404033085db6f38f6d67
Matthew Hensley <matthew.hensley@grafana.com>
2024-03-26 15:41:58 -0400

    add bare-metal host ID

--- src/SDK/Resource/Detectors/Host.php
+++ src/SDK/Resource/Detectors/Host.php
@@ -8,6 +8,7 @@ use OpenTelemetry\SDK\Common\Attribute\Attributes;
 use OpenTelemetry\SDK\Resource\ResourceDetectorInterface;
 use OpenTelemetry\SDK\Resource\ResourceInfo;
 use OpenTelemetry\SemConv\ResourceAttributes;
+use MachineIdSource;
 use function php_uname;
 
 /**
@@ -15,13 +16,128 @@ use function php_uname;
  */
 final class Host implements ResourceDetectorInterface
 {
+    private string $dir;
+
+    public function __construct(string $dir = '/')
+    {
+        $this->dir = $dir;
+    }
+
     public function getResource(): ResourceInfo
     {
         $attributes = [
             ResourceAttributes::HOST_NAME => php_uname('n'),
             ResourceAttributes::HOST_ARCH => php_uname('m'),
+            ResourceAttributes::HOST_ID => $this->getMachineId(),
         ];
 
         return ResourceInfo::create(Attributes::create($attributes), ResourceAttributes::SCHEMA_URL);
+    }
+
+    private function getMachineId()
+    {
+        switch (strtolower(PHP_OS_FAMILY))
+        {
+            case 'linux':
+            {
+                return $this->getLinuxId();
+            }
+            case 'freebsd':
+            case 'netbsd':
+            case 'openbsd':
+            {
+                return $this->getBsdId();
+            }
+            case 'darwin':
+            {
+                $out = $this->getMacOsId();
+                return Host::parseMacOsId($out);
+            }
+            case 'windows':
+            {
+                $out = $this->getWindowsId();
+                return Host::parseWindowsId($out);
+            }
+        }
+
+        return '';
+    }
+
+    private function getLinuxId(): string
+    {
+        $paths = ['etc/machine-id', 'var/lib/dbus/machine-id'];
+
+        foreach ($paths as $path)
+        {
+            if (file_exists($this->dir . $path))
+            {
+                return trim(file_get_contents($this->dir . $path));
+            }
+        }
+
+        return '';
+    }
+
+    private function getBsdId(): string
+    {
+        if (file_exists('/etc/hostid'))
+        {
+            return trim(file_get_contents('/etc/hostid'));
+        }
+
+        $out = exec('kenv -q smbios.system.uuid');
+
+        if ($out != FALSE)
+        {
+            return $out;
+        }
+
+        return '';
+    }
+
+    private function getMacOsId(): string
+    {
+        $out = exec('ioreg -rd1 -c "IOPlatformExpertDevice"');
+
+        if ($out != FALSE)
+        {
+            return $out;
+        }
+
+        return '';
+    }
+
+    private function getWindowsId(): string
+    {
+        $out = exec('%windir%\System32\REG.exe QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography /v MachineGuid');
+
+        if ($out != FALSE)
+        {
+            return $out;
+        }
+
+        return '';
+    }
+
+    public static function parseMacOsId(string $out): string
+    {
+        $lines = explode("\n", $out);
+
+        foreach ($lines as $line)
+        {
+            if (strpos($line, 'IOPlatformUUID') !== FALSE)
+            {
+                $parts = explode('=', $line);
+                return trim($parts[1]);
+            }
+        }
+
+        return '';
+    }
+
+    public static function parseWindowsId(string $out): string
+    {
+        $parts = explode('REG_SZ', $out);
+        return trim($parts[1]);
     }
 }
--- tests/Unit/SDK/Resource/Detectors/HostTest.php
+++ tests/Unit/SDK/Resource/Detectors/HostTest.php
@@ -6,6 +6,7 @@ namespace OpenTelemetry\Tests\Unit\SDK\Resource\Detectors;
 
 use OpenTelemetry\SDK\Resource\Detectors;
 use OpenTelemetry\SemConv\ResourceAttributes;
+use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -21,5 +22,60 @@ class HostTest extends TestCase
         $this->assertSame(ResourceAttributes::SCHEMA_URL, $resource->getSchemaUrl());
         $this->assertIsString($resource->getAttributes()->get(ResourceAttributes::HOST_NAME));
         $this->assertIsString($resource->getAttributes()->get(ResourceAttributes::HOST_ARCH));
+        $this->assertIsString($resource->getAttributes()->get(ResourceAttributes::HOST_ID));
+    }
+
+    public function test_host_parse_macos_id(): void
+    {
+        $out = 'IOPlatformUUID=1234567890';
+        $hostId = Detectors\Host::parseMacOsId($out);
+        $this->assertIsString($hostId);
+        $this->assertSame('1234567890', $hostId);
+    }
+
+    public function test_host_parse_windows_id(): void
+    {
+        $out = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography\MachineGuid    REG_SZ    1234567890';
+        $hostId = Detectors\Host::parseWindowsId($out);
+        $this->assertIsString($hostId);
+        $this->assertSame('1234567890', $hostId);
+    }
+
+    /**
+     * @dataProvider hostIdData
+     */
+    public function test_host_id_linux(array $files, string $expectedId): void
+    {
+        $root = vfsStream::setup('/', null, $files);
+        $resouceDetector = new Detectors\Host($root->url());
+        $resource = $resouceDetector->getResource();
+        $hostId = $resource->getAttributes()->get(ResourceAttributes::HOST_ID);
+        $this->assertIsString($hostId);
+        $this->assertSame($expectedId, $hostId);
+    }
+
+    public static function hostIdData(): array
+    {
+        $etc = [
+            'etc' => [
+              'machine-id' => '1234567890',
+            ]
+        ];
+        $varLibDbus = [
+            'var' => [
+                'lib' => [
+                    'dbus' => [
+                        'machine-id' => '0987654321',
+                    ],
+                ],
+            ],
+        ];
+
+        return [
+            [[], ''],
+            [$etc, '1234567890'],
+            [array_merge($etc, $varLibDbus), '1234567890'],
+            [$varLibDbus, '0987654321'],
+        ];
     }
 }
