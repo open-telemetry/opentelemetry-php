bb9eb40a5033abb2157994e087fdc6dd0a3cc559
Matt Hensley <130569+matt-hensley@users.noreply.github.com>
2024-04-01 18:20:09 -0400

    Add host.id for non-containerized systems (#1266)
    Co-authored-by: Tobias Bachert <git@b-privat.de>
    Co-authored-by: Chris Lightfoot-Wild <github-clw@wild.me.uk>

--- tests/Unit/SDK/Resource/Detectors/HostTest.php
+++ tests/Unit/SDK/Resource/Detectors/HostTest.php
@@ -6,6 +6,7 @@ namespace OpenTelemetry\Tests\Unit\SDK\Resource\Detectors;
 
 use OpenTelemetry\SDK\Resource\Detectors;
 use OpenTelemetry\SemConv\ResourceAttributes;
+use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -21,5 +22,57 @@ class HostTest extends TestCase
         $this->assertSame(ResourceAttributes::SCHEMA_URL, $resource->getSchemaUrl());
         $this->assertIsString($resource->getAttributes()->get(ResourceAttributes::HOST_NAME));
         $this->assertIsString($resource->getAttributes()->get(ResourceAttributes::HOST_ARCH));
+    }
+
+    /**
+     * @dataProvider hostIdData
+     */
+    public function test_host_id_filesystem(string $os, array $files, ?string $expectedId): void
+    {
+        $root = vfsStream::setup('/', null, $files);
+        $resouceDetector = new Detectors\Host($root->url(), $os);
+        $resource = $resouceDetector->getResource();
+
+        if ($expectedId === null) {
+            $this->assertFalse($resource->getAttributes()->has(ResourceAttributes::HOST_ID));
+
+            return;
+        }
+
+        $hostId = $resource->getAttributes()->get(ResourceAttributes::HOST_ID);
+        $this->assertIsString($hostId);
+        $this->assertSame($expectedId, $hostId);
+    }
+
+    public static function hostIdData(): array
+    {
+        $etc_machineid = [
+            'etc' => [
+                'machine-id' => '1234567890',
+            ],
+        ];
+        $varLibDbus = [
+            'var' => [
+                'lib' => [
+                    'dbus' => [
+                        'machine-id' => '0987654321',
+                    ],
+                ],
+            ],
+        ];
+        $etc_hostid = [
+            'etc' => [
+                'hostid' => '1234567890',
+            ],
+        ];
+
+        return [
+            ['Linux', [], null],
+            ['Linux', $etc_machineid, '1234567890'],
+            ['Linux', array_merge($etc_machineid, $varLibDbus), '1234567890'],
+            ['Linux', $etc_machineid, '1234567890'],
+            ['BSD', [], null],
+            ['BSD', $etc_hostid, '1234567890'],
+        ];
     }
 }
