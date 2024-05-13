373072ab53f7c912e7c7e5f4fa334150b06ecf26
Matthew Hensley <matthew.hensley@grafana.com>
2024-05-13 10:54:41 -0400

    implement experimental service.instance.id attribute

---
+++ src/SDK/Resource/Detectors/Service.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\SDK\Resource\Detectors;
+
+use OpenTelemetry\SDK\Common\Attribute\Attributes;
+use OpenTelemetry\SDK\Resource\ResourceDetectorInterface;
+use OpenTelemetry\SDK\Resource\ResourceInfo;
+use OpenTelemetry\SemConv\ResourceAttributes;
+use Ramsey\Uuid\Uuid;
+
+/**
+ * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/resource/sdk.md#specifying-resource-information-via-an-environment-variable
+ */
+final class Service implements ResourceDetectorInterface
+{
+    public function getResource(): ResourceInfo
+    {
+        $attributes[ResourceAttributes::SERVICE_INSTANCE_ID] = Uuid::uuid4()->toString();
+        return ResourceInfo::create(Attributes::create($attributes), ResourceAttributes::SCHEMA_URL);
+    }
+}
