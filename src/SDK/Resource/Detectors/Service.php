52d4697528766fdbfa24740d13d6cf68fe5bc24b
Matthew Hensley <matthew.hensley@grafana.com>
2024-05-13 21:07:14 -0400

    PR feedback

--- src/SDK/Resource/Detectors/Service.php
+++ src/SDK/Resource/Detectors/Service.php
@@ -8,6 +8,7 @@ use OpenTelemetry\SDK\Common\Attribute\Attributes;
 use OpenTelemetry\SDK\Resource\ResourceDetectorInterface;
 use OpenTelemetry\SDK\Resource\ResourceInfo;
 use OpenTelemetry\SemConv\ResourceAttributes;
+use \Ramsey\Uuid\Uuid;
 
 /**
  * @see https://github.com/open-telemetry/semantic-conventions/tree/main/docs/resource#service-experimental
@@ -16,11 +17,8 @@ final class Service implements ResourceDetectorInterface
 {
     public function getResource(): ResourceInfo
     {
-        static $serviceInstanceId = null;
-
-        if ($serviceInstanceId === null) {
-            $serviceInstanceId = \Ramsey\Uuid\Uuid::uuid4()->toString();
-        }
+        static $serviceInstanceId;
+        $serviceInstanceId ??= Uuid::uuid4()->toString();
 
         $attributes = [
             ResourceAttributes::SERVICE_INSTANCE_ID => $serviceInstanceId,
