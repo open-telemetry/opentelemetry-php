c0cd4ed5f8c9d3fc2b41004b0e4661bbce338486
Brett McBride <brett@deakin.edu.au>
2024-05-18 19:49:31 +1000

    more late binding test coverage

--- tests/Unit/API/Instrumentation/AutoInstrumentation/LateBindingProviderTest.php
+++ tests/Unit/API/Instrumentation/AutoInstrumentation/LateBindingProviderTest.php
@@ -12,6 +12,15 @@ use OpenTelemetry\API\Instrumentation\AutoInstrumentation\Context;
 use OpenTelemetry\API\Instrumentation\AutoInstrumentation\HookManager;
 use OpenTelemetry\API\Instrumentation\AutoInstrumentation\Instrumentation;
 use OpenTelemetry\API\Instrumentation\Configurator;
+use OpenTelemetry\API\Logs\LateBindingLogger;
+use OpenTelemetry\API\Logs\LateBindingLoggerProvider;
+use OpenTelemetry\API\Logs\LoggerInterface;
+use OpenTelemetry\API\Logs\LoggerProviderInterface;
+use OpenTelemetry\API\Logs\LogRecord;
+use OpenTelemetry\API\Metrics\LateBindingMeter;
+use OpenTelemetry\API\Metrics\LateBindingMeterProvider;
+use OpenTelemetry\API\Metrics\MeterInterface;
+use OpenTelemetry\API\Metrics\MeterProviderInterface;
 use OpenTelemetry\API\Trace\LateBindingTracer;
 use OpenTelemetry\API\Trace\LateBindingTracerProvider;
 use OpenTelemetry\API\Trace\TracerInterface;
@@ -20,10 +29,18 @@ use OpenTelemetry\SDK\Common\Configuration\Variables;
 use OpenTelemetry\SDK\SdkAutoloader;
 use OpenTelemetry\Tests\TestState;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\CoversMethod;
 use PHPUnit\Framework\TestCase;
 
+#[CoversClass(LateBindingLoggerProvider::class)]
+#[CoversClass(LateBindingLogger::class)]
+#[CoversClass(LateBindingMeterProvider::class)]
+#[CoversClass(LateBindingMeter::class)]
 #[CoversClass(LateBindingTracerProvider::class)]
 #[CoversClass(LateBindingTracer::class)]
+#[CoversMethod(SdkAutoloader::class, 'createLateBindingLoggerProvider')]
+#[CoversMethod(SdkAutoloader::class, 'createLateBindingMeterProvider')]
+#[CoversMethod(SdkAutoloader::class, 'createLateBindingTracerProvider')]
 class LateBindingProviderTest extends TestCase
 {
     use TestState;
@@ -33,7 +50,7 @@ class LateBindingProviderTest extends TestCase
         Logging::disable();
     }
 
-    public function test_late_binding_tracer(): void
+    public function test_late_binding_providers(): void
     {
         $instrumentation = new class() implements Instrumentation {
             private static ?Context $context;
@@ -47,29 +64,70 @@ class LateBindingProviderTest extends TestCase
 
                 return self::$context->tracerProvider->getTracer('test');
             }
+            public function getMeter(): MeterInterface
+            {
+                assert(self::$context !== null);
+
+                return self::$context->meterProvider->getMeter('test');
+            }
+            public function getLogger(): LoggerInterface
+            {
+                assert(self::$context !== null);
+
+                return self::$context->loggerProvider->getLogger('test');
+            }
         };
         $this->setEnvironmentVariable(Variables::OTEL_PHP_AUTOLOAD_ENABLED, 'true');
-        $called = false;
-        //the "real" tracer, which will be accessed through a late binding tracer
+        $tracer_called = false;
+        $logger_called = false;
+        $meter_called = false;
+        //the "real" tracer+meter+logger, which will be accessed through late binding providers
         $tracerProvider = $this->createMock(TracerProviderInterface::class);
-        $tracer = $this->createMock(TracerInterface::class);
-        $tracerProvider->method('getTracer')->willReturnCallback(function () use (&$called, $tracer): TracerInterface {
-            $called = true;
+        $tracerProvider->method('getTracer')->willReturnCallback(function () use (&$tracer_called): TracerInterface {
+            $tracer_called = true;
 
-            return $tracer;
+            return $this->createMock(TracerInterface::class);
+        });
+        $meterProvider = $this->createMock(MeterProviderInterface::class);
+        $meterProvider->method('getMeter')->willReturnCallback(function () use (&$meter_called): MeterInterface {
+            $meter_called = true;
+
+            return $this->createMock(MeterInterface::class);
+        });
+        $loggerProvider = $this->createMock(LoggerProviderInterface::class);
+        $loggerProvider->method('getLogger')->willReturnCallback(function () use (&$logger_called): LoggerInterface {
+            $logger_called = true;
+
+            return $this->createMock(LoggerInterface::class);
         });
         ServiceLoader::register(Instrumentation::class, $instrumentation::class);
         //@todo reset?
         $this->assertTrue(SdkAutoloader::autoload());
         //tracer initializer added _after_ autoloader has run and instrumentation registered
-        Globals::registerInitializer(function (Configurator $configurator) use ($tracerProvider): Configurator {
-            return $configurator->withTracerProvider($tracerProvider);
+        Globals::registerInitializer(function (Configurator $configurator) use ($tracerProvider, $loggerProvider, $meterProvider): Configurator {
+            return $configurator
+                ->withTracerProvider($tracerProvider)
+                ->withMeterProvider($meterProvider)
+                ->withLoggerProvider($loggerProvider)
+            ;
         });
 
-        $this->assertFalse($called);
+        $this->assertFalse($tracer_called);
         $tracer = $instrumentation->getTracer();
-        $this->assertFalse($called);
+        $this->assertFalse($tracer_called);
         $tracer->spanBuilder('test-span')->startSpan(); /** @phpstan-ignore-next-line */
-        $this->assertTrue($called);
+        $this->assertTrue($tracer_called);
+
+        $this->assertFalse($meter_called);
+        $meter = $instrumentation->getMeter();
+        $this->assertFalse($meter_called);
+        $meter->createCounter('cnt'); /** @phpstan-ignore-next-line */
+        $this->assertTrue($meter_called);
+
+        $this->assertFalse($logger_called);
+        $logger = $instrumentation->getLogger();
+        $this->assertFalse($logger_called);
+        $logger->emit(new LogRecord()); /** @phpstan-ignore-next-line */
+        $this->assertTrue($logger_called);
     }
 }
