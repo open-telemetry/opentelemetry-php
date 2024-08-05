c72d822c78e1d7fffed165e73918f913f1cdcd3f
Brett McBride <brett@deakin.edu.au>
2024-08-05 15:20:41 +1000

    mark LocalRootSpan as experimental

--- src/API/Trace/LocalRootSpan.php
+++ src/API/Trace/LocalRootSpan.php
@@ -8,18 +8,34 @@ use OpenTelemetry\Context\Context;
 use OpenTelemetry\Context\ContextInterface;
 use OpenTelemetry\Context\ContextKeyInterface;
 
+/**
+ * @experimental
+ */
 class LocalRootSpan
 {
+    /**
+     * Retrieve the local root span. This is the root-most active span which has
+     * a remote or invalid parent.
+     * If there is no active local root span, then an invalid span is returned.
+     * @experimental
+     */
     public static function current(): SpanInterface
     {
         return self::fromContext(Context::getCurrent());
     }
 
+    /**
+     * Retrieve the local root span from a Context.
+     * @experimental
+     */
     public static function fromContext(ContextInterface $context): SpanInterface
     {
         return $context->get(self::key()) ?? Span::getInvalid();
     }
 
+    /**
+     * @internal
+     */
     public static function store(ContextInterface $context, SpanInterface $span): ContextInterface
     {
         return $context->with(self::key(), $span);
