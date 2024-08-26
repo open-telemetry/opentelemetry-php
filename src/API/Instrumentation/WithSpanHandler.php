f4b52c4b493614fab31c5a9f5d653186dae8fc69
Brett McBride <brett.a.mcbride@gmail.com>
2024-08-26 11:17:16 +1000

    add minimal auto-instrumentation hook functions (#1358)
    add OpenTelemetry\API\Instrumentation\WithSpanHandler, which will be the default pre/post
    hook callbacks for attribute-based auto-instrumentation.

---
+++ src/API/Instrumentation/WithSpanHandler.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\API\Instrumentation;
+
+use OpenTelemetry\API\Trace\Span;
+use OpenTelemetry\API\Trace\SpanKind;
+use OpenTelemetry\API\Trace\StatusCode;
+use OpenTelemetry\Context\Context;
+use Throwable;
+
+/**
+ * Generic pre-hook and post-hook handlers for attribute-based auto instrumentation
+ */
+class WithSpanHandler
+{
+    /**
+     * @psalm-suppress ArgumentTypeCoercion
+     */
+    public static function pre(mixed $target, array $params, string $class, string $function, ?string $filename, ?int $lineno, ?array $span_args = [], ?array $attributes = []): void
+    {
+        static $instrumentation;
+        $instrumentation ??= new CachedInstrumentation(name: 'io.opentelemetry.php.with-span', schemaUrl: 'https://opentelemetry.io/schemas/1.25.0');
+
+        $name = $span_args['name'] ?? null;
+        if ($name === null) {
+            $name = empty($class)
+                ? $function
+                : sprintf('%s::%s', $class, $function);
+        }
+
+        $kind = $span_args['span_kind'] ?? SpanKind::KIND_INTERNAL;
+
+        $span = $instrumentation
+            ->tracer()
+            ->spanBuilder($name)
+            ->setSpanKind($kind)
+            ->setAttribute('code.function', $function)
+            ->setAttribute('code.namespace', $class)
+            ->setAttribute('code.filepath', $filename)
+            ->setAttribute('code.lineno', $lineno)
+            ->setAttributes($attributes ?? [])
+            ->startSpan();
+        $context = $span->storeInContext(Context::getCurrent());
+        Context::storage()->attach($context);
+    }
+
+    public static function post(mixed $target, array $params, mixed $result, ?Throwable $exception): void
+    {
+        $scope = Context::storage()->scope();
+        $scope?->detach();
+
+        if (!$scope || $scope->context() === Context::getCurrent()) {
+            return;
+        }
+
+        $span = Span::fromContext($scope->context());
+        if ($exception) {
+            $span->recordException($exception, ['exception.escaped' => true]);
+            $span->setStatus(StatusCode::STATUS_ERROR, $exception->getMessage());
+        }
+
+        $span->end();
+    }
+}
