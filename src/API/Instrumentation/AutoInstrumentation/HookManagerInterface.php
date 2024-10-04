b7eff275677fb5466fff2e82a0b23e6e864b97a7
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2024-10-04 18:25:35 +0100

    Update src/API/Instrumentation/AutoInstrumentation/HookManagerInterface.php
    Co-authored-by: Tobias Bachert <git@b-privat.de>

--- src/API/Instrumentation/AutoInstrumentation/HookManagerInterface.php
+++ src/API/Instrumentation/AutoInstrumentation/HookManagerInterface.php
@@ -11,7 +11,7 @@ interface HookManagerInterface
 {
     /**
      * @param ?Closure(object|string|null, array, ?string, string, ?string, ?int): (array|null|void) $preHook
-     * @param ?Closure(object|string|null, array, mixed, ?Throwable): mixed $postHook
+     * @param ?Closure(object|string|null, array, mixed, ?Throwable): (mixed|void) $postHook
      */
     public function hook(?string $class, string $function, ?Closure $preHook = null, ?Closure $postHook = null): void;
 }
