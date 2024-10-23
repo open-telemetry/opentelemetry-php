3481905b1a4e8a6a05fb5751ae48e137b671c11f
Brett McBride <brett@deakin.edu.au>
2024-10-23 16:00:02 +1100

    don't static init registry from SPI. add tests

--- tests/Unit/SDK/FactoryRegistryTest.php
+++ tests/Unit/SDK/FactoryRegistryTest.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\Tests\Unit\SDK;
 
+use Nevay\SPI\ServiceLoader;
 use OpenTelemetry\Context\Propagation\TextMapPropagatorInterface;
+use OpenTelemetry\SDK\Common\Attribute\Attributes;
 use OpenTelemetry\SDK\Common\Configuration\Variables;
 use OpenTelemetry\SDK\Common\Export\TransportFactoryInterface;
 use OpenTelemetry\SDK\Logs\LogRecordExporterFactoryInterface;
 use OpenTelemetry\SDK\Metrics\MetricExporterFactoryInterface;
 use OpenTelemetry\SDK\Registry;
+use OpenTelemetry\SDK\Resource\ResourceDetectorFactoryInterface;
 use OpenTelemetry\SDK\Resource\ResourceDetectorInterface;
+use OpenTelemetry\SDK\Resource\ResourceInfo;
 use OpenTelemetry\SDK\Trace\SpanExporter\SpanExporterFactoryInterface;
 use OpenTelemetry\Tests\TestState;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -147,10 +151,44 @@ class FactoryRegistryTest extends TestCase
         ];
     }
 
-    public function test_retrieve_from_composer_extra(): void
+    public function test_retrieve_from_spi(): void
     {
         $this->setEnvironmentVariable(Variables::OTEL_PHP_EXPERIMENTAL_SPI_REGISTRY, 'true');
         $this->assertFileExists(dirname(__DIR__, 3) . '/vendor/composer/GeneratedServiceProviderData.php');
         $this->assertInstanceOf(ResourceDetectorInterface::class, Registry::resourceDetector('test'));
+    }
+
+    public function test_add_to_spi(): void
+    {
+        $this->setEnvironmentVariable(Variables::OTEL_PHP_EXPERIMENTAL_SPI_REGISTRY, 'true');
+        $factory = new class() implements ResourceDetectorFactoryInterface {
+            public function create(): ResourceDetectorInterface
+            {
+                return new class() implements ResourceDetectorInterface {
+                    public function getResource(): ResourceInfo
+                    {
+                        $attributes = [
+                            'foo-resource' => 'test-value',
+                        ];
+
+                        return ResourceInfo::create(Attributes::create(['foo-resource' => 'foo']));
+                    }
+                };
+            }
+            public function type(): string
+            {
+                return 'foo';
+            }
+            public function priority(): int
+            {
+                return 99;
+            }
+        };
+        ServiceLoader::register(ResourceDetectorFactoryInterface::class, $factory::class);
+
+        $detector = Registry::resourceDetector('foo');
+        $this->assertInstanceOf(ResourceDetectorInterface::class, $detector);
+        $this->assertTrue($detector->getResource()->getAttributes()->has('foo-resource'));
+        $this->assertSame('foo', $detector->getResource()->getAttributes()->get('foo-resource'));
     }
 }
