bb75c6c26869880a2e424d739fb789d30150158b
Brett McBride <brett@deakin.edu.au>
2024-12-19 16:38:25 +1100

    disable error handler instead of catch

--- src/SDK/Resource/Detectors/Host.php
+++ src/SDK/Resource/Detectors/Host.php
@@ -9,7 +9,6 @@ use OpenTelemetry\SDK\Resource\ResourceDetectorInterface;
 use OpenTelemetry\SDK\Resource\ResourceInfo;
 use OpenTelemetry\SemConv\ResourceAttributes;
 use function php_uname;
-use Throwable;
 
 /**
  * @see https://github.com/open-telemetry/opentelemetry-specification/blob/v1.8.0/specification/resource/semantic_conventions/host.md#host
@@ -48,6 +47,27 @@ final class Host implements ResourceDetectorInterface
         };
     }
 
+    /**
+     * @phan-suppress PhanTypeMismatchArgumentInternal
+     */
+    private function readFile(string $file): string|false
+    {
+        set_error_handler(static fn () => true);
+
+        try {
+            if (is_file($file) && is_readable($file)) {
+                $contents = file_get_contents($file);
+                if ($contents !== false) {
+                    return $contents;
+                }
+            }
+        } finally {
+            restore_error_handler();
+        }
+
+        return false;
+    }
+
     private function getLinuxId(): ?string
     {
         $paths = [self::PATH_ETC_MACHINEID, self::PATH_VAR_LIB_DBUS_MACHINEID];
@@ -55,18 +75,9 @@ final class Host implements ResourceDetectorInterface
         foreach ($paths as $path) {
             $file = $this->dir . $path;
 
-            try {
-                if (is_file($file) && is_readable($file)) {
-                    $contents = file_get_contents($file);
-
-                    if ($contents === false) {
-                        continue;
-                    }
-
-                    return trim($contents);
-                }
-            } catch (Throwable $t) {
-                //do nothing
+            $contents = $this->readFile($file);
+            if ($contents !== false) {
+                return $contents;
             }
         }
 
@@ -77,16 +88,9 @@ final class Host implements ResourceDetectorInterface
     {
         $file = $this->dir . self::PATH_ETC_HOSTID;
 
-        try {
-            if (is_file($file) && is_readable($file)) {
-                $contents = file_get_contents($file);
-
-                if ($contents !== false) {
-                    return trim($contents);
-                }
-            }
-        } catch (Throwable $t) {
-            //do nothing
+        $contents = $this->readFile($file);
+        if ($contents !== false) {
+            return $contents;
         }
 
         $out = exec('which kenv && kenv -q smbios.system.uuid');
