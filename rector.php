0eb41f20bb73a42e0b3dbd578bc3d2d8180236e2
Brett McBride <brett@deakin.edu.au>
2024-12-23 17:05:44 +1100

    update to php 8.2 minimum

--- rector.php
+++ rector.php
@@ -7,15 +7,15 @@ use Rector\CodeQuality\Rector\ClassMethod\LocallyCalledStaticMethodToNonStaticRe
 use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
 use Rector\Config\RectorConfig;
-use Rector\Php81\Rector\ClassMethod\NewInInitializerRector;
-use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+//use Rector\Php81\Rector\ClassMethod\NewInInitializerRector;
+//use Rector\Php82\Rector\Property\ReadOnlyPropertyRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
 use Rector\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->phpVersion(PhpVersion::PHP_81);
+    $rectorConfig->phpVersion(PhpVersion::PHP_82);
 
     $rectorConfig->paths([
         __DIR__ . '/src',
@@ -23,19 +23,19 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->sets([
-        SetList::PHP_81,
+        SetList::PHP_82,
         SetList::CODE_QUALITY,
         PHPUnitSetList::PHPUNIT_100,
     ]);
     $rectorConfig->skip([
         FlipTypeControlToUseExclusiveTypeRector::class,
-        NewInInitializerRector::class => [
+        /*NewInInitializerRector::class => [
             __DIR__ . '/src/SDK/Trace/Sampler/ParentBased.php',
         ],
         ReadOnlyPropertyRector::class => [
             __DIR__ . '/src/SDK/Metrics/Stream/SynchronousMetricStream.php',
             __DIR__ . '/tests/Unit/Extension/Propagator',
-        ],
+        ],*/
         DisallowedEmptyRuleFixerRector::class,
         ExplicitBoolCompareRector::class,
         LocallyCalledStaticMethodToNonStaticRector::class,
