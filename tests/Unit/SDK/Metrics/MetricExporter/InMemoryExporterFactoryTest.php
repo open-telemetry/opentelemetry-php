a8fc516c7b117e30acadd86835c70f7f2ed0dfce
Brett McBride <brett@deakin.edu.au>
2025-01-13 15:26:56 +1100

    factory test coverage

--- tests/Unit/SDK/Metrics/MetricExporter/InMemoryExporterFactoryTest.php
+++ tests/Unit/SDK/Metrics/MetricExporter/InMemoryExporterFactoryTest.php
@@ -17,4 +17,16 @@ class InMemoryExporterFactoryTest extends TestCase
         $exporter = (new InMemoryExporterFactory())->create();
         $this->assertInstanceOf(MetricExporterInterface::class, $exporter);
     }
+
+    public function test_type(): void
+    {
+        $factory = new InMemoryExporterFactory();
+        $this->assertSame('memory', $factory->type());
+    }
+
+    public function test_priority(): void
+    {
+        $factory = new InMemoryExporterFactory();
+        $this->assertSame(0, $factory->priority());
+    }
 }
