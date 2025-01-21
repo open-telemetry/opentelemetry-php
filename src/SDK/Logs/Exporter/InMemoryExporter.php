516d8d9a5d86b2df471eba60594cba9391509dbd
Brett McBride <brett@deakin.edu.au>
2025-01-21 17:03:13 +1100

    implement mutable ReadWriteLogRecord https://github.com/open-telemetry/opentelemetry-specification/pull/3907 implements some new requirements for logging: - ReadWriteLogRecord can mutate (eg by processors) - mutated ReadWriteLogRecord can be seen by later processors
    This is a breaking change because LogRecordProcessorInterface onEmit param changes to by-reference

--- src/SDK/Logs/Exporter/InMemoryExporter.php
+++ src/SDK/Logs/Exporter/InMemoryExporter.php
@@ -9,6 +9,7 @@ use OpenTelemetry\SDK\Common\Future\CancellationInterface;
 use OpenTelemetry\SDK\Common\Future\CompletedFuture;
 use OpenTelemetry\SDK\Common\Future\FutureInterface;
 use OpenTelemetry\SDK\Logs\LogRecordExporterInterface;
+use OpenTelemetry\SDK\Logs\ReadableLogRecord;
 
 class InMemoryExporter implements LogRecordExporterInterface
 {
@@ -22,7 +23,7 @@ class InMemoryExporter implements LogRecordExporterInterface
     public function export(iterable $batch, ?CancellationInterface $cancellation = null): FutureInterface
     {
         foreach ($batch as $record) {
-            $this->storage->append($record);
+            $this->storage->append($this->convert($record));
         }
 
         return new CompletedFuture(true);
@@ -41,5 +42,20 @@ class InMemoryExporter implements LogRecordExporterInterface
     public function getStorage(): ArrayObject
     {
         return $this->storage;
+    }
+
+    private function convert(ReadableLogRecord $record): array
+    {
+        return [
+            'timestamp' => $record->getTimestamp(),
+            'observed_timestamp' => $record->getObservedTimestamp(),
+            'severity_number' => $record->getSeverityNumber(),
+            'severity_text' => $record->getSeverityText(),
+            'body' => $record->getBody(),
+            'attributes' => $record->getAttributes()->toArray(),
+            'trace_id' => $record->getSpanContext()?->getTraceId(),
+            'span_id' => $record->getSpanContext()?->getSpanId(),
+            'trace_flags' => $record->getSpanContext()?->getTraceFlags(),
+        ];
     }
 }
