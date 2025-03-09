0ffdd29cb3b5455882db40f595fe3d55b06f7976
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2025-03-09 20:46:55 +0000

    Porting EnvSourceProvider changes from Nevay.

---
+++ src/Config/SDK/Configuration/Environment/LazyEnvSource.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\Config\SDK\Configuration\Environment;
+
+use Closure;
+
+/**
+ * @internal
+ */
+final class LazyEnvSource implements EnvSource
+{
+    /**
+     * @param Closure(): EnvSource|EnvSource $env
+     */
+    public function __construct(
+        private Closure|EnvSource $env,
+    ) {
+    }
+
+    public function readRaw(string $name): mixed
+    {
+        if (!$this->env instanceof EnvSource) {
+            $this->env = ($this->env)();
+        }
+
+        return $this->env->readRaw($name);
+    }
+}
