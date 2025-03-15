cfc5075be47d77903d005eeece27d09e11168900
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2025-03-15 00:34:00 +0000

    Added SdkConfigurationResolver.

---
+++ src/SDK/Common/Configuration/Resolver/SdkConfigurationResolver.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\SDK\Common\Configuration\Resolver;
+
+use Nevay\SPI\ServiceLoader;
+use Nevay\SPI\ServiceProviderDependency\PackageDependency;
+use OpenTelemetry\Config\SDK\Configuration\Environment\EnvSourceProvider;
+use OpenTelemetry\Config\SDK\Configuration\Environment\EnvSourceReader;
+use OpenTelemetry\Config\SDK\Configuration\Environment\LazyEnvSource;
+use OpenTelemetry\Config\SDK\Configuration\Environment\PhpIniEnvSource;
+use OpenTelemetry\Config\SDK\Configuration\Environment\ServerEnvSource;
+use OpenTelemetry\SDK\Common\Configuration\Configuration;
+
+/**
+ * @internal
+ */
+#[PackageDependency('open-telemetry/sdk-configuration', '*')]
+class SdkConfigurationResolver implements ResolverInterface
+{
+    private readonly EnvSourceReader $reader;
+
+    public function __construct()
+    {
+        $envSources = [];
+        $envSources[] = new ServerEnvSource();
+        $envSources[] = new PhpIniEnvSource();
+
+        /** @var EnvSourceProvider $envSourceProvider */
+        foreach (ServiceLoader::load(EnvSourceProvider::class) as $envSourceProvider) {
+            $envSources[] = new LazyEnvSource($envSourceProvider->getEnvSource(...));
+        }
+
+        $this->reader = new EnvSourceReader($envSources);
+    }
+
+    public function retrieveValue(string $variableName)
+    {
+        return $this->reader->read($variableName);
+    }
+
+    public function hasVariable(string $variableName): bool
+    {
+        return !Configuration::isEmpty($this->reader->read($variableName));
+    }
+}
