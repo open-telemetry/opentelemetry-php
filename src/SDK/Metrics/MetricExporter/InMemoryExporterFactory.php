39a2b486b1b03f7ebc608f507e3aebf04a8ce7b5
Grzegorz Drozd <1885137+GrzegorzDrozd@users.noreply.github.com>
2025-04-15 09:02:07 +0200

    Refactor InMemory exporters to use shared storage manager (#1559)
    Introduced `InMemoryStorageManager` to centralize storage management for spans, metrics, and logs. Updated InMemory exporters and their factory classes to utilize this shared storage.
    
    * Add integration test for InMemoryExporter
    
    - add integration test that shows that storage contains metrics
    - because of src/SDK/Metrics/MetricReader/ExportingReader.php:148 I had to add `PushMetricExporterInterface` to InMemoryExporter class
    
    * Add integration test for InMemoryExporter

--- src/SDK/Metrics/MetricExporter/InMemoryExporterFactory.php
+++ src/SDK/Metrics/MetricExporter/InMemoryExporterFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\SDK\Metrics\MetricExporter;
 
+use OpenTelemetry\SDK\Common\Export\InMemoryStorageManager;
 use OpenTelemetry\SDK\Metrics\MetricExporterFactoryInterface;
 use OpenTelemetry\SDK\Metrics\MetricExporterInterface;
 
@@ -11,6 +12,6 @@ class InMemoryExporterFactory implements MetricExporterFactoryInterface
 {
     public function create(): MetricExporterInterface
     {
-        return new InMemoryExporter();
+        return new InMemoryExporter(InMemoryStorageManager::metrics());
     }
 }
--- src/SDK/Trace/SpanExporter/InMemorySpanExporterFactory.php
+++ src/SDK/Trace/SpanExporter/InMemorySpanExporterFactory.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\SDK\Trace\SpanExporter;
 
+use OpenTelemetry\SDK\Common\Export\InMemoryStorageManager;
 use OpenTelemetry\SDK\Trace\SpanExporterInterface;
 
 class InMemorySpanExporterFactory implements SpanExporterFactoryInterface
 {
     public function create(): SpanExporterInterface
     {
-        return new InMemoryExporter();
+        return new InMemoryExporter(InMemoryStorageManager::spans());
     }
 }
