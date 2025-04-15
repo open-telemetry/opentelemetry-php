93b57c6580d6d5b0bdc71ff3602936f39f5d7f4f
Brett McBride <brett@deakin.edu.au>
2025-04-15 15:05:34 +1000

    add memory exporters

--- src/Config/SDK/ComponentProvider/Logs/LogRecordExporterMemory.php
+++ src/Config/SDK/ComponentProvider/Logs/LogRecordExporterMemory.php
@@ -2,25 +2,25 @@
 
 declare(strict_types=1);
 
-namespace OpenTelemetry\Config\SDK\ComponentProvider\Trace;
+namespace OpenTelemetry\Config\SDK\ComponentProvider\Logs;
 
 use OpenTelemetry\Config\SDK\Configuration\ComponentProvider;
 use OpenTelemetry\Config\SDK\Configuration\ComponentProviderRegistry;
 use OpenTelemetry\Config\SDK\Configuration\Context;
-use OpenTelemetry\SDK\Trace\SpanExporter\InMemoryExporter;
-use OpenTelemetry\SDK\Trace\SpanExporterInterface;
+use OpenTelemetry\SDK\Logs\Exporter\InMemoryExporter;
+use OpenTelemetry\SDK\Logs\LogRecordExporterInterface;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 
 /**
- * @implements ComponentProvider<SpanExporterInterface>
+ * @implements ComponentProvider<LogRecordExporterInterface>
  */
-final class SpanExporterMemory implements ComponentProvider
+final class LogRecordExporterMemory implements ComponentProvider
 {
     /**
      * @param array{} $properties
      */
-    public function createPlugin(array $properties, Context $context): SpanExporterInterface
+    public function createPlugin(array $properties, Context $context): LogRecordExporterInterface
     {
         return new InMemoryExporter();
     }
--- src/Config/SDK/ComponentProvider/Metrics/MetricExporterMemory.php
+++ src/Config/SDK/ComponentProvider/Metrics/MetricExporterMemory.php
@@ -2,25 +2,25 @@
 
 declare(strict_types=1);
 
-namespace OpenTelemetry\Config\SDK\ComponentProvider\Trace;
+namespace OpenTelemetry\Config\SDK\ComponentProvider\Metrics;
 
 use OpenTelemetry\Config\SDK\Configuration\ComponentProvider;
 use OpenTelemetry\Config\SDK\Configuration\ComponentProviderRegistry;
 use OpenTelemetry\Config\SDK\Configuration\Context;
-use OpenTelemetry\SDK\Trace\SpanExporter\InMemoryExporter;
-use OpenTelemetry\SDK\Trace\SpanExporterInterface;
+use OpenTelemetry\SDK\Metrics\MetricExporter\InMemoryExporter;
+use OpenTelemetry\SDK\Metrics\MetricExporterInterface;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 
 /**
- * @implements ComponentProvider<SpanExporterInterface>
+ * @implements ComponentProvider<MetricExporterInterface>
  */
-final class SpanExporterMemory implements ComponentProvider
+final class MetricExporterMemory implements ComponentProvider
 {
     /**
      * @param array{} $properties
      */
-    public function createPlugin(array $properties, Context $context): SpanExporterInterface
+    public function createPlugin(array $properties, Context $context): MetricExporterInterface
     {
         return new InMemoryExporter();
     }
