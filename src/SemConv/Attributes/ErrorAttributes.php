dddc3da0581ccf8a019f04befb188aa2af942a77
Brett McBride <brett@deakin.edu.au>
2025-07-07 15:58:42 +1000

    refactor semantic conventions Align with how the spec advises SemConv should be organised, and break unstable semconvs out into their own directory. I played around with generating values as enums, but I could only get this to work if the output was in the same file as the attributes (which would not work well with composer autoloading), so I've used <attribute>_VALUE_<name>, similar to what we have now except all in the same file.

---
+++ src/SemConv/Attributes/ErrorAttributes.php
@@ -0,0 +1,49 @@
+<?php
+
+// DO NOT EDIT, this is an Auto-generated file from script/semantic-conventions
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\SemConv\Attributes;
+
+/**
+ * Semantic attributes and corresponding values for error.
+ * @see https://opentelemetry.io/docs/specs/semconv/registry/attributes/error/
+ */
+interface ErrorAttributes
+{
+    /**
+     * Describes a class of error the operation ended with.
+     *
+     * The `error.type` SHOULD be predictable, and SHOULD have low cardinality.
+     *
+     * When `error.type` is set to a type (e.g., an exception type), its
+     * canonical class name identifying the type within the artifact SHOULD be used.
+     *
+     * Instrumentations SHOULD document the list of errors they report.
+     *
+     * The cardinality of `error.type` within one instrumentation library SHOULD be low.
+     * Telemetry consumers that aggregate data from multiple instrumentation libraries and applications
+     * should be prepared for `error.type` to have high cardinality at query time when no
+     * additional filters are applied.
+     *
+     * If the operation has completed successfully, instrumentations SHOULD NOT set `error.type`.
+     *
+     * If a specific domain defines its own set of error identifiers (such as HTTP or gRPC status codes),
+     * it's RECOMMENDED to:
+     *
+     * - Use a domain-specific attribute
+     * - Set `error.type` to capture all errors, regardless of whether they are defined within the domain-specific set or not.
+     *
+     * @stable
+     */
+    public const ERROR_TYPE = 'error.type';
+
+    /**
+     * A fallback error value to be used when the instrumentation doesn't define a custom value.
+     *
+     * @stable
+     */
+    public const ERROR_TYPE_VALUE_OTHER = '_OTHER';
+
+}
---
+++ src/SemConv/Attributes/ExceptionAttributes.php
@@ -0,0 +1,36 @@
+<?php
+
+// DO NOT EDIT, this is an Auto-generated file from script/semantic-conventions
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\SemConv\Attributes;
+
+/**
+ * Semantic attributes and corresponding values for exception.
+ * @see https://opentelemetry.io/docs/specs/semconv/registry/attributes/exception/
+ */
+interface ExceptionAttributes
+{
+    /**
+     * The exception message.
+     *
+     * @stable
+     */
+    public const EXCEPTION_MESSAGE = 'exception.message';
+
+    /**
+     * A stacktrace as a string in the natural representation for the language runtime. The representation is to be determined and documented by each language SIG.
+     *
+     * @stable
+     */
+    public const EXCEPTION_STACKTRACE = 'exception.stacktrace';
+
+    /**
+     * The type of the exception (its fully-qualified class name, if applicable). The dynamic type of the exception should be preferred over the static type in languages that support it.
+     *
+     * @stable
+     */
+    public const EXCEPTION_TYPE = 'exception.type';
+
+}
