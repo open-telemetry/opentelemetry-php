b789fff88a11e7388c3e89987cb0ee0df9f81edd
Jerry Leung <tingfung.leung@solarwinds.com>
2025-11-14 09:44:33 -0800

    Reverted and added comments

--- src/SDK/Logs/EventLogger.php
+++ src/SDK/Logs/EventLogger.php
@@ -40,6 +40,11 @@ class EventLogger implements EventLoggerInterface
         iterable $attributes = [],
     ): void {
         $logRecord = new LogRecord();
+        /**
+         * This attribute is intended to set twice to ensure event.name is the first attribute entry and won't be dropped by attribute limits.
+         * @see https://github.com/open-telemetry/opentelemetry-php/pull/1768#issuecomment-3527425474
+         */
+        $logRecord->setAttribute('event.name', $name);
         $logRecord->setAttributes($attributes);
         $logRecord->setAttribute('event.name', $name);
         $logRecord->setBody($body);
