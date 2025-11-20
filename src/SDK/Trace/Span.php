690c9d8144533d2d919055ac1f83fb6951363a6e
Jerry Leung <tingfung.leung@solarwinds.com>
2025-11-20 14:00:15 -0800

    Updated

--- src/SDK/Trace/Span.php
+++ src/SDK/Trace/Span.php
@@ -273,6 +273,9 @@ final class Span extends API\Span implements ReadWriteSpanInterface
         }
 
         $this->endEpochNanos = $endEpochNanos ?? Clock::getDefault()->now();
+        if ($this->spanProcessor instanceof ExtendedSpanProcessorInterface) {
+            $this->spanProcessor->OnEnding($this);
+        }
         $this->hasEnded = true;
 
         $this->checkForDroppedElements();
